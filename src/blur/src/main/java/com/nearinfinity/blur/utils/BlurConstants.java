8e73f9699644f8000d622fb22fea73feb4448f90
amccurry <amccurry@gmail.com>
2010-09-14 02:15:54 +0000

    git-svn-id: https://scm.nearinfinity.com/svn/incubator/blur/trunk@17563 5c733ba6-9949-11dd-800e-6d5850b5fe75

--- src/blur/src/main/java/com/nearinfinity/blur/utils/BlurConstants.java
+++ src/blur/src/main/java/com/nearinfinity/blur/utils/BlurConstants.java
@@ -2,8 +2,12 @@ package com.nearinfinity.blur.utils;
 
 import java.util.Comparator;
 
+import com.nearinfinity.blur.manager.hits.HitsComparator;
+import com.nearinfinity.blur.manager.hits.HitsPeekableIteratorComparator;
+import com.nearinfinity.blur.manager.hits.PeekableIterator;
 import com.nearinfinity.blur.thrift.generated.Column;
 import com.nearinfinity.blur.thrift.generated.ColumnFamily;
+import com.nearinfinity.blur.thrift.generated.Hit;
 
 
 public interface BlurConstants {
@@ -20,4 +24,6 @@ public interface BlurConstants {
 	public static final String SHARD = "shard";
 	public static final Comparator<? super ColumnFamily> COLUMN_FAMILY_COMPARATOR = new ColumnFamilyComparator();
 	public static final Comparator<? super Column> COLUMN_COMPARATOR = new ColumnComparator();
+    public static final Comparator<? super PeekableIterator<Hit>> HITS_PEEKABLE_ITERATOR_COMPARATOR = new HitsPeekableIteratorComparator();
+    public static final Comparator<? super Hit> HITS_COMPARATOR = new HitsComparator();
 }

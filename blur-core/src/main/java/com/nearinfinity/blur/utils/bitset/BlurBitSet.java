40dfa260cd8b01c6e7b1108471bf376f7499bc8d
aavenoso <aavenoso@nearinfinity.com>
2011-03-02 01:17:40 +0000

    BLUR-47 sped up the bitset prev method
    git-svn-id: https://scm.nearinfinity.com/svn/incubator/blur/trunk@18644 5c733ba6-9949-11dd-800e-6d5850b5fe75

--- blur-core/src/main/java/com/nearinfinity/blur/utils/bitset/BlurBitSet.java
+++ blur-core/src/main/java/com/nearinfinity/blur/utils/bitset/BlurBitSet.java
@@ -39,8 +39,36 @@ public class BlurBitSet extends OpenBitSet {
     public int prevSetBit(int index) {
         return (int) prevSetBit((long) index);
     }
-
     public long prevSetBit(long index) {
+    	if (index < 0) {
+            return -1;
+        }
+    	int initialWordNum = (int)(index >> 6);
+    	if (initialWordNum == -1) {
+    		initialWordNum = Integer.MAX_VALUE;
+        }
+    	int wordNum = initialWordNum;
+    	if(wordNum > wlen - 1) {
+    		wordNum = wlen - 1;
+    	}
+    	while(wordNum >= 0) {
+    		long word = bits[wordNum];
+    		if(word != 0) {
+    			if(wordNum == initialWordNum){
+    				int offset = ((int)(index & 0x3F));
+    				long mask = -1L >>> (63-offset);
+    				word = word & mask;
+    			}
+    			if(word != 0) {
+    				return (((long)wordNum)<<6) + (63-BitUtil.ntz(Long.reverse(word)));
+    			}
+    		}
+    		wordNum--;
+    	}
+    	return -1;
+    }
+
+    public long prevSetBitOld(long index) {
         //do not use yet
         
         if (index < 0) {
--- blur-core/src/test/java/com/nearinfinity/blur/utils/BlurBitSetTest.java
+++ blur-core/src/test/java/com/nearinfinity/blur/utils/BlurBitSetTest.java
@@ -1,79 +1,136 @@
 package com.nearinfinity.blur.utils;
 
-import static junit.framework.Assert.fail;
+import static junit.framework.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import com.nearinfinity.blur.utils.bitset.BlurBitSet;
 
 public class BlurBitSetTest {
+	private BlurBitSet bits;
 
-//    @Test
-	public void testRandomBlurBitSet() {
-		BlurBitSet blurBitSet = new BlurBitSet();
+	@Before
+	public void setUp() {
+		bits = new BlurBitSet();
+	}
+
+	@Test
+	public void testPreviousHitBeginning() {
+		assertEquals(-1, bits.prevSetBit(30));
+		bits.set(50);
+		assertEquals(-1, bits.prevSetBit(30));
+	}
+
+	@Test
+	public void testPreviousBlurBitSet() {
+		bits.set(10);
+		assertEquals(10, bits.prevSetBit(14));
+	}
+
+	@Test
+	public void testPreviousSmallBitLargeSearch() {
+		bits.set(10);
+		assertEquals(10, bits.prevSetBit(5000));
+	}
+
+	@Test
+	public void testPreviousLargeBit() {
+		bits.set(200);
+		assertEquals(200, bits.prevSetBit(400));
+		assertEquals(-1, bits.prevSetBit(100));
+	}
+
+	@Test
+	public void testSmallRandomBlurBitSet() {
 		long seed = getSeed();
-		System.out.println("Running with seed [" + seed + "]");
-        populate(blurBitSet,1000000,100000,new Random(seed));
-		
+		System.out.println("testSmallRandomBlurBitSet Running with seed [" + seed + "]");
+		populate(bits, 100, 10, new Random(seed));
+
 		List<Long> setBits = new ArrayList<Long>();
 		long bit = -1;
-		while ((bit = blurBitSet.nextSetBit(bit + 1)) != -1) {
+		while ((bit = bits.nextSetBit(bit + 1)) != -1) {
 			setBits.add(bit);
 		}
-		
+
 		bit = Long.MAX_VALUE;
-		while ((bit = blurBitSet.prevSetBit(bit - 1)) != -1) {
-			if (!setBits.remove((Long)bit)) {
+		while ((bit = bits.prevSetBit(bit - 1)) != -1) {
+			if (!setBits.remove((Long) bit)) {
 				fail("Bit [" + Long.toString(bit) + "] was not found.");
 			}
 		}
+		assertEquals(0, setBits.size());
 	}
-	
-    private long getSeed() {
-        return new Random().nextLong();
-    }
-
-//    @Test
-	public void testPerformance() {
-		BlurBitSet blurBitSet = new BlurBitSet();
+	@Test
+	public void testRandomBlurBitSet() {
 		long seed = getSeed();
-        System.out.println("Running with seed [" + seed + "]");
-		populate(blurBitSet,1000000,10000,new Random(seed));
-		
+		System.out.println("testRandomBlurBitSet Running with seed [" + seed + "]");
+		populate(bits, 1000000, 10000, new Random(seed));
+
+		List<Long> setBits = new ArrayList<Long>();
+		long bit = -1;
+		while ((bit = bits.nextSetBit(bit + 1)) != -1) {
+			setBits.add(bit);
+		}
+
+		bit = Long.MAX_VALUE;
+		while ((bit = bits.prevSetBit(bit - 1)) != -1) {
+			if (!setBits.remove((Long) bit)) {
+				fail("Bit [" + Long.toString(bit) + "] was not found.");
+			}
+		}
+		assertEquals(0, setBits.size());
+	}
+
+	private long getSeed() {
+		return new Random().nextLong();
+	}
+
+	@Test
+	public void testPerformance() {
+		long seed = getSeed();
+		System.out.println("testPerformance Running with seed [" + seed + "]");
+		populate(bits, 1000000, 1000, new Random(seed));
+
 		long nextTime = 0;
 		long prevTime = 0;
-		
+
 		for (int i = 0; i < 100; i++) {
 			long total1 = 0;
 			long bit = -1;
 			long s1 = System.nanoTime();
-			while ((bit = blurBitSet.nextSetBit(bit + 1)) != -1) {
+			while ((bit = bits.nextSetBit(bit + 1)) != -1) {
 				total1 += bit;
 			}
 			long e1 = System.nanoTime();
-			nextTime += (e1-s1);
-			
+			nextTime += (e1 - s1);
+
 			long total2 = 0;
 			bit = Long.MAX_VALUE;
 			long s2 = System.nanoTime();
-			while ((bit = blurBitSet.prevSetBit(bit - 1)) != -1) {
+			while ((bit = bits.prevSetBit(bit - 1)) != -1) {
 				total2 += bit;
 			}
 			long e2 = System.nanoTime();
-			prevTime += (e2-s2);
+			prevTime += (e2 - s2);
+			System.out.print('.');
+			assertEquals(total1, total2);
 		}
-		
-		System.out.println("Next time [" + nextTime +
-				"] Prev Time [" + prevTime +
-				"]");
+		System.out.println("");
+
+		System.out.println("Next time [" + nextTime + "] Prev Time ["
+				+ prevTime + "]");
 	}
 
-	private void populate(BlurBitSet blurBitSet, int maxSize, int maxPopulation, Random random) {
+	private void populate(BlurBitSet blurBitSet, int maxSize,
+			int maxPopulation, Random random) {
 		while (blurBitSet.cardinality() < maxPopulation) {
 			blurBitSet.set(random.nextInt(maxSize));
 		}
 	}
-	
+
 }

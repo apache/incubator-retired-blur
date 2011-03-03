f2197867ba39b9ba19670e5c099534fa32e776da
amccurry <amccurry@gmail.com>
2011-03-03 02:11:06 +0000

    Making use of the preSetBit in the SuperScorer.
    git-svn-id: https://scm.nearinfinity.com/svn/incubator/blur/trunk@18653 5c733ba6-9949-11dd-800e-6d5850b5fe75

--- blur-core/src/main/java/com/nearinfinity/blur/lucene/search/SuperQuery.java
+++ blur-core/src/main/java/com/nearinfinity/blur/lucene/search/SuperQuery.java
@@ -232,18 +232,19 @@ public class SuperQuery extends AbstractWrapperQuery {
 		}
 
 		private int getPrimeDoc(int doc) {
-//		    System.out.println("================================");
-//		    System.out.println("doc=" + doc);
-//		    System.out.println("prevSetBit [" + bitSet.prevSetBit(doc) + "]");
-			while (!bitSet.get(doc)) {
-				doc--;
-				if (doc <= 0) {
-//				    System.out.println("prime [" + 0 + "] but really [" + doc + "]");
-					return 0;
-				}
-			}
-//			System.out.println("prime [" + doc + "]");
-			return doc;
+		    return bitSet.prevSetBit(doc);
+////		    System.out.println("================================");
+////		    System.out.println("doc=" + doc);
+////		    System.out.println("prevSetBit [" + bitSet.prevSetBit(doc) + "]");
+//			while (!bitSet.get(doc)) {
+//				doc--;
+//				if (doc <= 0) {
+////				    System.out.println("prime [" + 0 + "] but really [" + doc + "]");
+//					return 0;
+//				}
+//			}
+////			System.out.println("prime [" + doc + "]");
+//			return doc;
 		}
 
 		private boolean isScorerExhausted(int doc) {

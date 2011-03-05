87a62fff6bad030fddbabb5812f589e01f5fbedc
Aaron McCurry <amccurry@nic-amccurry.home>
2011-03-04 22:51:45 -0500

    Adding license header to each file.

--- blur-core/src/test/java/com/nearinfinity/blur/manager/SelectorIndexManagerTest.java
+++
@@ -1,5 +0,0 @@
-package com.nearinfinity.blur.manager;
-
-public class SelectorIndexManagerTest {
-
-}
--- blur-store/src/main/java/com/nearinfinity/blur/store/HdfsDirectory.java
+++ blur-store/src/main/java/com/nearinfinity/blur/store/HdfsDirectory.java
@@ -1,14 +1,11 @@
-package com.nearinfinity.blur.store;
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+/*
+ * Copyright (C) 2011 Near Infinity Corporation
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +14,7 @@ package com.nearinfinity.blur.store;
  * limitations under the License.
  */
 
+package com.nearinfinity.blur.store;
 
 import java.io.IOException;
 import java.util.ArrayList;

94cbdeebaaf8e312863c580876f63b3a2eac5138
Chris Rohr <crohr@nearinfinity.com>
2011-05-05 22:51:52 -0400

    Initial commit for various blur tools

---
+++ blur-admin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlurAdmin::Application.config.secret_token = 'f2ad453ed6cd3f79e8c300ef557de0d3985a0e03007d871975dd9f9c19e8f7b86563db38175f139f368d13e8987eae97d4be79ddb2fa37a399a10da7254df625'

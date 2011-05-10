5114ad520f444020174fd7642e7d721aba6b0a9a
Matt Rohr <mrohr@nearinfinity.com>
2011-05-10 15:54:55 -0400

    Initial layout

--- blur-admin/public/javascripts/application.js
+++ blur-admin/public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function(){
+    function menuSetup() {               
+        $('#menu-bar > ul > li > a').each(function() {
+            var content = $(this).next().html();
+            if (content) {
+                $(this).menu({
+                    content: content,
+                    flyOut: true,
+										maxHeight: 100
+               });
+               $(this).hover(function(){$(this).parent().toggleClass('ui-state-hover')});
+            }
+        });
+    }
+    menuSetup();
+});

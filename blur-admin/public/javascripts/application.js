b59c7102a100bc521104bde3ec2303e28b72f0b5
Matt Rohr <mrohr@nearinfinity.com>
2011-05-12 09:23:40 -0400

    finished initial interface for admin tools

--- blur-admin/public/javascripts/application.js
+++ blur-admin/public/javascripts/application.js
@@ -1,8 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
-    function menuSetup() {               
-        $('#menu-bar > ul > li > a').each(function() {
+    function menuSetup() {  
+	      var length = $('#menu-bar > ul > li > a').length;
+        $('#menu-bar > ul > li > a').each(function(index) {
             var content = $(this).next().html();
             if (content) {
                 $(this).menu({
@@ -11,8 +12,18 @@ $(function(){
 										maxHeight: 100
                });
                $(this).hover(function(){$(this).parent().toggleClass('ui-state-hover')});
+							if(index ==0)
+							{
+								$(this).addClass('ui-corner-left');
+							}
+							
+							if(index ==length -1)
+							{
+								$(this).addClass('ui-corner-right');								
+							}
             }
         });
     }
     menuSetup();
+		$('#widgets').sortable();
 });

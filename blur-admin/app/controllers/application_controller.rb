a20f72e689c0dc19c8c4807e1503748d9a7352a0
Matt Rohr <mrohr@nearinfinity.com>
2011-05-18 15:34:08 -0400

    Moved the thrift setup and close methods to the applicationcontroller

--- blur-admin/app/controllers/application_controller.rb
+++ blur-admin/app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def setup_thrift
+    @transport = Thrift::FramedTransport.new(Thrift::BufferedTransport.new(Thrift::Socket.new('blur04.nearinfinity.com', 40020)))
+    protocol = Thrift::BinaryProtocol.new(@transport)
+    client = Com::Nearinfinity::Blur::Thrift::Generated::Blur::Client.new(protocol)
+    @transport.open()
+    
+    client
+    
+  rescue Thrift::TransportException
+    client = nil
+    client
+  end
+  
+  def close_thrift
+    @transport.close()
+  end
 end

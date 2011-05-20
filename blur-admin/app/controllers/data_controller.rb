3a7c01cbf0b2075dac2465002824f0abf12a1df7
Jamie Swogger <jswogger@nearinfinity.com>
2011-05-20 11:17:16 -0400

    merged files

--- blur-admin/app/controllers/data_controller.rb
+++ blur-admin/app/controllers/data_controller.rb
@@ -1,5 +1,18 @@
 class DataController < ApplicationController
+
+  require 'thrift/blur'
   def show
+    client = setup_thrift
+    @tables = client.tableList()
+    close_thrift
+  end
+
+  def curr_tables
+    client = setup_thrift
+    curr_tables = client.tableList()
+    close_thrift
+
+    render :json => curr_tables
   end
 
 end

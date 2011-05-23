5d676751fb6d3cad6d0f08685456a7791f399d45
Jamie Swogger <jswogger@nearinfinity.com>
2011-05-23 11:43:25 -0400

    new features to data page

--- blur-admin/app/controllers/data_controller.rb
+++ blur-admin/app/controllers/data_controller.rb
@@ -4,31 +4,13 @@ class DataController < ApplicationController
   def show
     client = setup_thrift
     @tables = client.tableList()
-    
+    @tdesc = Hash.new
+    @tschema = Hash.new
+    @tables.each do |table|
+      @tdesc[table] = client.describe(table)
+      @tschema[table] = client.schema(table).columnFamilies
+    end
     close_thrift
   end
-
-  def curr_tables
-    client = setup_thrift
-    curr_tables = client.tableList()
-    close_thrift
-
-    render :json => curr_tables
-  end
-
-  #def get_shards(table)
-    #TODO: get the shards
-    #return 'placeholder'
-  #end
-
-  #def get_size(table)
-    #TODO: get the size
-    #return 'placeholder'
-  #end
-
-  #def get_count(table)
-    #TODO: get the count
-    #return 'placeholder'
-  #end
 
 end

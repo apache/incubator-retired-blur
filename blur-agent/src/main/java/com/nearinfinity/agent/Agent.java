b5241a1af10e5d310ab298ad7610aec4b45da648
Chris Rohr <crohr@nearinfinity.com>
2011-05-22 21:53:56 -0400

    Initial checkin of agent code

--- blur-agent/src/main/java/com/nearinfinity/agent/Agent.java
+++ blur-agent/src/main/java/com/nearinfinity/agent/Agent.java
@@ -1,9 +1,38 @@
 package com.nearinfinity.agent;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+import com.nearinfinity.agent.collectors.HDFSCollector;
+import com.nearinfinity.agent.collectors.LoggerCollector;
+
 public class Agent {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws ParseException {
+		Options options = new Options();
+		options.addOption("H", false, "Turns on HDFS monitoring, HDFS uri required");
+		options.addOption("l", true, "Turns on Logger server, Logger port to listen on required");
+		
+		//TODO: Figure out how to configure mysql instance
+		
+		CommandLineParser parser = new PosixParser();
+		CommandLine cmd = parser.parse(options, args);
+		
+		new Agent(cmd);
+	}
+	
+	public Agent(CommandLine cmd) {
+		if (cmd.hasOption('H')) {
+//			HDFSCollector.startCollecting(cmd.getOptionValue('H'));
+			HDFSCollector.startCollecting();
+		}
+		
+		if (cmd.hasOption('l')) {
+			LoggerCollector.startCollecting(cmd.getOptionValue('l'));
+		}
 	}
 
 }
---
+++ blur-agent/src/main/java/com/nearinfinity/agent/collectors/LoggerCollector.java
@@ -0,0 +1,7 @@
+package com.nearinfinity.agent.collectors;
+
+public class LoggerCollector {
+	public static void startCollecting(String port) {
+		
+	}
+}

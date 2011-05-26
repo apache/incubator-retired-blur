9a8926c84daf7bb136671ad918b53093d88e2ee0
Jamie Swogger <jswogger@nearinfinity.com>
2011-05-26 15:12:04 -0400

    fix for window-resize issue: can change in screen.scss by altering width

--- blur-admin/spec/helpers/data_helper.rb
+++ blur-admin/spec/helpers/data_helper.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe DataHelper do
   describe "#get_shards" do
-    it "returns the sharts" do
+    it "returns the shards" do
 			#TODO
       #helper.get_shards().should eq("...")
     end

51c5bec5b6809fbc4f65ce963bc47501e2744c15
Jamie Swogger <jswogger@nearinfinity.com>
2011-05-27 15:42:49 -0400

    data and runtime page clean up

--- blur-admin/app/controllers/runtime_controller.rb
+++ blur-admin/app/controllers/runtime_controller.rb
@@ -3,11 +3,12 @@ class RuntimeController < ApplicationController
   def show
     client = setup_thrift
     @tables = client.tableList()
+    @running_queries = BlurQueries.where(:table_name => 'employee_super_mart').all
     close_thrift
   end
 
   def current_queries
-    #TODO: get current table for quereies
+    #TODO: get current table for queries
     #logger.info "*** getting queries for #{params[:table]} ***"
     #@running_queries = BlurQueries.where("table_name = ?", params[:table]).all
     #render render :json => @running_queries

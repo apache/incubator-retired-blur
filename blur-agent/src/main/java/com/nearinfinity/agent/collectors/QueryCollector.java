190b2ddf6d963a92075da18024f66e406fba0920
Chris Rohr <crohr@nearinfinity.com>
2011-06-02 22:13:10 -0400

    Added user id column to blur queries

--- blur-agent/src/main/java/com/nearinfinity/agent/collectors/QueryCollector.java
+++ blur-agent/src/main/java/com/nearinfinity/agent/collectors/QueryCollector.java
@@ -46,6 +46,7 @@ public class QueryCollector {
 						| post_filters             | mediumtext   | YES  |     | NULL    |                |
 						| selector_column_families | text         | YES  |     | NULL    |                |
 						| selector_columns         | text         | YES  |     | NULL    |                |
+						| userid                   | varchar(255) | YES  |     | NULL    |                |
 						+--------------------------+--------------+------+-----+---------+----------------+
 
 
@@ -61,7 +62,7 @@ public class QueryCollector {
 							if (existingRow.isEmpty()) {
 								System.out.println("Inserting new query");
 //								System.out.println("Start time:" + blurQueryStatus.getQuery().get);
-								jdbc.update("insert into blur_queries (query_string, cpu_time, real_time, complete, interrupted, running, uuid, created_at, table_name, super_query_on, facets, start, fetch_num, pre_filters, post_filters, selector_column_families, selector_columns) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", 
+								jdbc.update("insert into blur_queries (query_string, cpu_time, real_time, complete, interrupted, running, uuid, created_at, table_name, super_query_on, facets, start, fetch_num, pre_filters, post_filters, selector_column_families, selector_columns, userid) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", 
 											new Object[]{blurQueryStatus.getQuery().getQueryStr(), 
 												blurQueryStatus.getCpuTime(),
 												blurQueryStatus.getRealTime(),
@@ -79,7 +80,8 @@ public class QueryCollector {
 												blurQueryStatus.getQuery().getPreSuperFilter(),
 												blurQueryStatus.getQuery().getPostSuperFilter(),
 												blurQueryStatus.getQuery().getSelector() == null ? null : JSONValue.toJSONString(blurQueryStatus.getQuery().getSelector().getColumnFamiliesToFetch()),
-												blurQueryStatus.getQuery().getSelector() == null ? null : JSONValue.toJSONString(blurQueryStatus.getQuery().getSelector().getColumnsToFetch())
+												blurQueryStatus.getQuery().getSelector() == null ? null : JSONValue.toJSONString(blurQueryStatus.getQuery().getSelector().getColumnsToFetch()),
+												blurQueryStatus.getQuery().getUserId()
 											});
 							} else if ((Integer)existingRow.get(0).get("COMPLETE") != 1){
 								System.out.println("Updating existing query");

3dc499db0c195ab492c02b22211dfe2506f99158
Chris Rohr <crohr@nearinfinity.com>
2011-06-04 09:33:49 -0400

    Added code coverage

--- blur-admin/spec/spec_helper.rb
+++ blur-admin/spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'cover_me'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'

92afdc973d654a9abd9dfc865ce08442fb1024ef
Dan Burkert <dburkert@nearinfinity.com>
2011-06-06 10:13:11 -0400

    cleaned upp data helpers and view as per code review

--- blur-admin/app/helpers/data_helper.rb
+++ blur-admin/app/helpers/data_helper.rb
@@ -1,23 +1,12 @@
 module DataHelper
-  def get_shards(table)
+
+  def shards(table)
     hosts = {}
     @tserver[table].each do |shard,host|
       hosts[host] = [] unless hosts.has_key? host
       hosts[host] << shard
     end
     hosts
-  end
-
-  def get_count(table)
-		number_with_delimiter(@tcount[table], :delimiter => ',')
-  end
-
-  def get_a_blur_table(table)
-    BlurTables.where(:table_name => table).first
-  end
-
-  def get_loc(table)
-    @tdesc[table].tableUri
   end
 
 end

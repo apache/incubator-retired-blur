3cd41e96f63453bc1b817f734672280175618090
Chris Rohr <crohr@nearinfinity.com>
2011-06-09 14:28:33 -0400

    updates to the tests

--- blur-admin/app/controllers/data_controller.rb
+++ blur-admin/app/controllers/data_controller.rb
@@ -1,5 +1,4 @@
 class DataController < ApplicationController
-  before_filter :setup_thrift
   before_filter :table_name, :only => [:update, :destroy_table]
   after_filter :close_thrift
 
@@ -7,7 +6,6 @@ class DataController < ApplicationController
     bq = Blur::BlurQuery.new :queryStr => '*', :fetch => 1, :superQueryOn => false
     @tables = thrift_client.tableList.sort
     
-    puts @tables.inspect
     @tdesc = {}
     @tschema = {}
     @tserver = {}

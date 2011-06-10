4a4ed09b0d4f159e2a0a05a6cc3f8358f7526b94
Jamie Swogger <jswogger@nearinfinity.com>
2011-06-10 13:28:02 -0400

    application controller spec

---
+++ blur-admin/spec/controllers/application_controller_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+describe ApplicationController do
+  describe "thrift_client" do
+    it "makes the setup_thrift call if thrift_client is nil" do
+      
+    end
+  end
+end

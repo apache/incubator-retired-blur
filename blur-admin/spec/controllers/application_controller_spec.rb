5252aebad5a65f45bfbb8e3ada6cd78a82a993ec
Jamie Swogger <jswogger@nearinfinity.com>
2011-06-10 14:38:40 -0400

    new column on runtime page

--- blur-admin/spec/controllers/application_controller_spec.rb
+++ blur-admin/spec/controllers/application_controller_spec.rb
@@ -2,8 +2,6 @@ require "spec_helper"
 
 describe ApplicationController do
   describe "thrift_client" do
-    it "makes the setup_thrift call if thrift_client is nil" do
-      
-    end
+    it "makes the setup_thrift call if thrift_client is nil"
   end
 end

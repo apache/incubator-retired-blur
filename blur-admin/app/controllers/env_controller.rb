74224e1ba6d7167db78890f42f33372833d238c6
Jamie Swogger <jswogger@nearinfinity.com>
2011-06-13 15:55:55 -0400

    ui fixes & runtime tests

--- blur-admin/app/controllers/env_controller.rb
+++ blur-admin/app/controllers/env_controller.rb
@@ -1,10 +1,10 @@
 class EnvController < ApplicationController
 
+  after_filter :close_thrift
+
   def show
-    setup_thrift
-    @controllers = @client.controllerServerList
-    @shards = @client.shardServerList
-    close_thrift
+    @controllers = thrift_client.controllerServerList
+    @shards = thrift_client.shardServerList
   end
 
 end

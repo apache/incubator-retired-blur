da8f443a46ed2a91f12f36abd6d031da50a43e9e
Jamie Swogger <jswogger@nearinfinity.com>
2011-06-14 10:59:35 -0400

    test?

--- blur-admin/spec/controllers/application_controller_spec.rb
+++ blur-admin/spec/controllers/application_controller_spec.rb
@@ -1,11 +1,21 @@
 require "spec_helper"
 
 describe ApplicationController do
+  describe "setup_thrift" do
+    it "rescues from a TransportException error" do
+      host = 'string'
+      port = 'string'
+      @transport = Thrift::FramedTransport.new(Thrift::BufferedTransport.new(Thrift::Socket.new(BLUR_THRIFT[host], BLUR_THRIFT[port])))
+      controller.stub!(:new).and_return(@transport)
+      @client = mock(Blur::Blur::Client)
+      #@transport.open()
+    end
+  end
+
   describe "thrift_client" do
     it "makes the setup_thrift call if @client" do
       @client = mock(Blur::Blur::Client)
       controller.stub!(:setup_thrift)
-      #get :thrift_client
     end
   end
 end

eb039cd862df09bb82a2faf1845ad9593c129424
Dan Burkert <dburkert@nearinfinity.com>
2011-06-16 17:42:09 -0400

    Fixed status on runtime page, testing for BlurQuery model

--- blur-admin/app/models/blur_queries.rb
+++ blur-admin/app/models/blur_queries.rb
@@ -1,4 +1,18 @@
 class BlurQueries < ActiveRecord::Base
+
+  def shards
+    begin
+      shard_list = BlurThriftClient.client.shardServerLayout self.table_name
+      shard_list.length
+    rescue => exception
+      puts exception
+      puts "Exception in BlurQueries.shards"
+      nil
+    end
+  end
+
+
+
   def cancel
     begin
       BlurThriftClient.client.cancelQuery self.table_name, self.uuid.to_i 

dc2bf573f2363e53d501200b61f1bae532257ed1
Brandon Marc-Aurele <bmarcaur@nearinfinity.co>
2011-06-16 11:21:28 -0400

    more tests

--- blur-admin/spec/models/blur_tables_spec.rb
+++ blur-admin/spec/models/blur_tables_spec.rb
@@ -28,4 +28,36 @@ describe BlurTables do
       @blur_tables.table_analyzer.should == "blah"
     end
   end
+  
+  describe "schema" do
+    
+    before(:each) do
+      @table_schema = Blur::Schema.new :columnFamilies => {:family => "tree"}
+      @client.stub!(:schema).with('blah').and_return(@table_schema)
+    end
+    
+    it "given a schema check that shards returns the formatted hash" do
+      @table_server = {'a_shard' => 'a_host'}
+      @returned_host = {'a_host'=>['a_shard']}
+      @blur_tables.stub!(:server).and_return(@table_server)
+      @blur_tables.shards.should == @returned_host
+    end
+    
+    it "method returns the proper schema" do
+      @blur_tables.schema.should == {:family => "tree"}
+    end
+    
+  end
+  
+  describe "server" do
+    
+    before(:each) do
+      @client.stub!(:shardServerLayout).with('blah').and_return("blah")
+    end
+    
+    it "method returns the proper server list" do
+      @blur_tables.server.should == "blah"
+    end
+    
+  end
 end

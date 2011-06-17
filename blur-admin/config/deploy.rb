bf8d264a78242d1e3ca77cbf32aef5117e909c39
Chris Rohr <crohr@nearinfinity.com>
2011-06-16 21:44:44 -0400

    Finalizing capistrano

--- blur-admin/config/deploy.rb
+++ blur-admin/config/deploy.rb
@@ -1,9 +1,15 @@
-set :application, "blur-admin"
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+set :rvm_type, :user
+
+require 'bundler/capistrano'
+
 set :repository,  "git@scm.nearinfinity.com:blur-tools.git"
-
+set :branch, 'develop'
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+set :application, "blur-admin"
 set :deploy_to, "/home/localadmin/blur-tools"
 
 role :web, "nic-blurtest01.nearinfinity.com"                          # Your HTTP server, Apache/etc
@@ -12,16 +18,16 @@ role :db,  "nic-blurtest01.nearinfinity.com", :primary => true # This is where R
 
 set :user, "localadmin"
 set :use_sudo, false
-set :migrate_env, :test
+set :deploy_subdir, "blur-admin"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end

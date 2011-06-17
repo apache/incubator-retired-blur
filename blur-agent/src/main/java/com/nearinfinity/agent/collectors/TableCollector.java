f81cc4bbfdd9a9a66bd249258bb991eafba46cd9
Brandon Marc-Aurele <bmarcaur@nearinfinity.co>
2011-06-17 15:54:53 -0400

    more relevant print statements in agent

--- blur-agent/src/main/java/com/nearinfinity/agent/collectors/TableCollector.java
+++ blur-agent/src/main/java/com/nearinfinity/agent/collectors/TableCollector.java
@@ -42,7 +42,7 @@ public class TableCollector {
 		BlurClientManager.execute(connection, new BlurCommand<Void>() {
 			@Override
 			public Void call(Client client) throws Exception {
-				System.out.println("Updating MYSQL");
+				System.out.println("Checking For Updates in 'blur_tables'");
 				List<String> tables = client.tableList();
 				//mapper used to generate the json
 				ObjectMapper mapper = new ObjectMapper();

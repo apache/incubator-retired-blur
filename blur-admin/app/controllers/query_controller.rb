e3927d134c5938eeb7c53e64531941451a7301d3
Jamie Swogger <jswogger@nearinfinity.com>
2011-06-23 14:12:42 -0400

    add record id to query page results table

--- blur-admin/app/controllers/query_controller.rb
+++ blur-admin/app/controllers/query_controller.rb
@@ -4,7 +4,7 @@ class QueryController < ApplicationController
 	  @tables = BlurThriftClient.client.tableList.sort!
 	  @columns = BlurThriftClient.client.schema(@tables.first) unless @tables.blank?
 	end
-	
+
 	def filters
 	  @columns = BlurThriftClient.client.schema(params[:table])
 	  render '_filters.html.haml', :layout=>false
@@ -12,7 +12,7 @@ class QueryController < ApplicationController
 
 	def create
 		# TODO: Add in fetch filter
-		
+
 	  table = params[:t]
 		bq = Blur::BlurQuery.new :queryStr => params[:q], :fetch => params[:r].to_i, :start => params[:a].to_i, :uuid => Time.now.to_i*1000+rand(1000)
     if !params[:s]
@@ -28,6 +28,7 @@ class QueryController < ApplicationController
         parts = value.split('_')
         if (!columns.has_key?(parts[1]))
           columns[parts[1]] = []
+          columns[parts[1]] << 'record id'
         end
         columns[parts[1]] << parts[2]
       end
@@ -43,7 +44,10 @@ class QueryController < ApplicationController
 
     families_with_columns = {}
     families.each do |family|
-      families_with_columns[family] = BlurThriftClient.client.schema(table).columnFamilies[family]
+      families_with_columns[family] = ['record id']
+      BlurThriftClient.client.schema(table).columnFamilies[family].each do |family_name|
+        families_with_columns[family] << family_name
+      end
     end
 
     visible_families = (families + columns.keys).uniq
@@ -51,7 +55,6 @@ class QueryController < ApplicationController
     @result_count = blur_results.totalResults
     @result_time = blur_results.realTime
     blur_results.results.each do |result|
-      record_id = result.fetchResult.recordResult
       row = result.fetchResult.rowResult.row
       max_record_count = row.columnFamilies.collect {|cf| cf.records.keys.count }.max
 
@@ -69,12 +72,20 @@ class QueryController < ApplicationController
               if families_include
                 families_with_columns[column_family_name].each do |column|
                   found_set = column_family.records.values[record_count].find { |col| column == col.name }
+                    if !(column == 'record id')
                       cfspan << (found_set.nil? ? ' ' : found_set.values.join(', '))
+                    else
+                      cfspan << column_family.records.keys[record_count]
+                    end
                 end
               else
                 columns[column_family_name].each do |column|
                   found_set = column_family.records.values[record_count].find { |col| column == col.name }
+                    if !(column == 'record id')
                       cfspan << (found_set.nil? ? ' ' : found_set.values.join(', '))
+                    else
+                      cfspan << column_family.records.keys[record_count]
+                    end
                 end
               end
             else
@@ -94,13 +105,13 @@ class QueryController < ApplicationController
         end
       end
 
-      record = {:id => row.id, :recordid => record_id, :max_record_count => max_record_count, :row => row, :table_rows => table_rows}
-      
+      record = {:id => row.id, :max_record_count => max_record_count, :row => row, :table_rows => table_rows}
+
       @results << record
     end
-		
+
     @all_columns = families_with_columns.merge columns
-		
+
 		render :template=>'query/create.html.haml', :layout => false
 	end
 end

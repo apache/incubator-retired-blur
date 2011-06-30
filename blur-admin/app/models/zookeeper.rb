869a4abda9f217546e459959f50ec92a4f145902
Dan Burkert <dburkert@nearinfinity.com>
2011-06-30 11:48:01 -0400

    Factory Girl model definitions

--- blur-admin/app/models/zookeeper.rb
+++ blur-admin/app/models/zookeeper.rb
@@ -1,6 +1,6 @@
 class Zookeeper < ActiveRecord::Base
   set_table_name "blur_zookeeper_instances"
-  has_many :controllers, :foreign_key => "blur_zookeeper_id"
-  has_many :clusters, :foreign_key => "blur_zookeeper_id"
+  has_many :controllers, :foreign_key => "blur_zookeeper_instance_id"
+  has_many :clusters,    :foreign_key => "blur_zookeeper_instance_id"
   has_many :shards, :through => :clusters
 end
--- blur-admin/spec/factories.rb
+++ blur-admin/spec/factories.rb
@@ -8,49 +8,69 @@
 # Basic model definitions
 
 Factory.define :zookeeper do |t|
-
-
+  t.sequence (:name) { |n| "Test Zookeeper ##{n}" }
+  t.sequence (:url)  { |n| "zookeeper#{n}.blur.example.com" }
+  t.status           { rand 2 }
 end
 
-Factory.define :controller do |t| end
-
-Factory.define :cluster do |t| end
-
-Factory.define :shard do |t| end
-
-Factory.define :blur_table do |t| end
-
-Factory.define :blur_query do |t| end
-
-# More fully defined models
-
-Factory.define :blur_table_with_schema, :parent => :blur_table do |t|
-  t.table_name { "Blur Table ##{Factory.next :blur_table_name}" }
-  t.table_schema {|blur_table| "{\"table\":\"#{blur_table.table_name}\",\"setTable\":true,\"setColumnFamilies\":true,\"columnFamiliesSize\":2,\"columnFamilies\":{\"Column Family #1\":[\"Column #1\",\"Column #2\",\"Column #3\"],\"Column Family #1\":[\"Column #1\",\"Column #2\",\"Column #3\"]}}" }
-  
+Factory.define :controller do |t|
+  t.sequence (:node_name)     { |n| "Test Node ##{n}" }
+  t.sequence (:node_location) { |n| "node#{n}.blur.example.com" }
+  t.status                    { rand 2 }
+  t.blur_version              { "1.#{rand 10}.#{rand 10}" }
 end
 
+Factory.define :cluster do |t|
+  t.sequence (:name) { |n| "Test Cluster ##{n}" }
+end
+
+Factory.define :shard do |t|
+  t.blur_version              { "1.#{rand 10}.#{rand 10}" }
+  t.sequence (:node_name)     { |n| "Test Node ##{n}" }
+  t.sequence (:node_location) { |n| "node#{n}.blur.example.com" }
+  t.status                    { rand 2 }
+end
+
+Factory.define :blur_table do |t|
+  t.sequence (:table_name) { |n| "Test Blur Table ##{n}" }
+  t.current_size           { 10**12 + rand(999 * 10 ** 12) } #Between a terrabyte and a petabyte
+  t.query_usage            { rand 100 }                      #No idea what the range is like here...
+  t.record_count           { 10**6 + rand(999 * 10 ** 6) }   #Between a million and a billion 
+  t.status                 { rand 2 }
+  t.sequence (:table_uri)  { |n| "blur_table#{n}.blur.example.com" }
+  t.table_analyzer 'standard.table_analyzer'
+  t.table_schema { |blur_table| "{\"table\":\"#{blur_table.table_name}\",\"setTable\":true,\"setColumnFamilies\":true,\"columnFamiliesSize\":2,\"columnFamilies\":{\"Column Family #1\":[\"Column #1\",\"Column #2\",\"Column #3\"],\"Column Family #1\":[\"Column #1\",\"Column #2\",\"Column #3\"]}}" }
+end
+
+Factory.define :blur_query do |t|
+  t.sequence (:query_string) { |n| "Blur Query ##{n} Query String" }
+  t.cpu_time  { rand 10 * 10 ** 3 } #Between 0 and 10 seconds
+  t.real_time { |blur_query| blur_query.cpu_time + rand( 10 * 10 ** 3) } #Between 0 and 10 additional seconds
+  t.complete  { rand 2 }
+  t.interrupted { rand(5) == 0 } # 20% chance
+  t.running     { rand(5) == 0 } # 20% chance
+  t.uuid        { rand 10 ** 8 }
+  t.super_query_on { rand(4) != 0 } # 75% chance
+  t.start { rand 10 ** 6 }
+  t.fetch_num { rand 10 ** 6 }
+  t.userid { "Test User ##{rand 20}" }
+  #t.selector_column_families
+  #t.selector_columns
+  #t.pre_filters
+  #t.post_filters
+end
 
 # Create models with association chains already created. These  create create real objects and
 # persist them in the database
-Factory.define :zookeeper_with_controller, :parent => :zookeeper  do |t|
+
+Factory.define :zookeeper_with_cluster, :parent => :zookeeper do |t|
   t.after_create { |zookeeper| Factory.create(:controller, :zookeeper => zookeeper) }
+  t.after_create { |zookeeper| Factory.create(:cluster, :zookeeper => zookeeper) }
 end
 
-Factory.define :zookeeper_with_controllers, :parent => :zookeeper do |t|
+Factory.define :zookeeper_with_clusters, :parent => :zookeeper do |t|
   t.after_create { |zookeeper| @recursive_factor.times {Factory.create(:controller, :zookeeper => zookeeper)} }
-end
-
-Factory.define :zookeeper_with_cluster, :parent => :zookeeper_with_controller  do |t|
-  t.after_create do |zookeeper|
-    zookeeper.controllers.each { |controller| Factory.create(:cluster, :controller => controller) }
-  end
-end
-
-Factory.define :zookeeper_with_clusters, :parent => :zookeeper_with_controllers  do |t|
-  t.after_create do |zookeeper|
-    zookeeper.controllers.each { |controller| @recursive_factor.times {Factory.create(:cluster, :controller => controller)} }
-  end
+  t.after_create { |zookeeper| @recursive_factor.times {Factory.create(:cluster,    :zookeeper => zookeeper)} }
 end
 
 Factory.define :zookeeper_with_shard, :parent => :zookeeper_with_cluster  do |t|
@@ -71,9 +91,4 @@ end
 
 Factory.define :blur_table_with_blur_queries, :parent => :blur_table do |t|
   t.after_create { |blur_table| @recursive_factor.times {Factory.create(:blur_query, :blur_table => blur_table)} } 
-end
-
-# Sequences
-Factory.sequence :blur_table_name do |n|
-  n
 end

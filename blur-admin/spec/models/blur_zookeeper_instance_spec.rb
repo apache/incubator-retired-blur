9c15263e1cfb2c020146c5766a13634a856e2bba
Brandon Marc-Aurele <bmarcaur@gmail.com>
2011-07-11 14:07:48 -0400

    added a test for the blur_queries method

--- blur-admin/spec/models/blur_zookeeper_instance_spec.rb
+++ blur-admin/spec/models/blur_zookeeper_instance_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Zookeeper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "blur_queries" do
+    before :each do
+      @table = Factory.stub :blur_table
+      @query = Factory.stub :blur_query
+      @zoo = Zookeeper.new
+      @table.stub(:blur_queries).and_return([@query])
+      @zoo.stub(:blur_tables).and_return([@table, @table])
+    end
+    
+    it "uses the tables to return all of the queries associated with them" do
+      @zoo.blur_queries.should == [@query, @query]
+    end
+  
+  end 
 end

e703abac7b78a63c9a452e030175235c204cba75
Jamie Swogger <jswogger@nearinfinity.com>
2011-07-12 15:58:41 -0400

    background change & new spinner

--- blur-admin/app/controllers/zookeepers_controller.rb
+++ blur-admin/app/controllers/zookeepers_controller.rb
@@ -17,7 +17,6 @@ class ZookeepersController < ApplicationController
   def index
     time = Time.zone.now - 1.minutes
     @old_queries = BlurQuery.where ['created_at < ? and running = 1', time]
-    puts @old_queries.inspect
   end
 
   def make_current

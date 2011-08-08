a2d0cfa4b631d664ad4ec02b51c335141a829b6c
Dan Burkert <dburkert@nearinfinity.com>
2011-08-08 11:08:23 -0400

    blur_table model tests

--- blur-admin/spec/models/blur_table_spec.rb
+++ blur-admin/spec/models/blur_table_spec.rb
@@ -5,16 +5,13 @@ describe BlurTable do
   before(:each) do
     @client = mock(ThriftClient)
     BlurThriftClient.stub!(:client).and_return(@client)
-    @table = BlurTable.new :table_name =>    'blah',
-                                  :status =>        "2",
-                                  :server =>  "{\"Host1:101\":[\"shard-001\", \"shard-002\", \"shard-003\"], \"Host2:102\":[\"shard-004\", \"shard-005\", \"shard-006\"]}"
+    @table = Factory.create :blur_table
   end
   
   describe "enable " do
-    
     it "method sends the message to enable the table through thrift" do
-      @client.should_receive(:enableTable).with('blah')
-      @table.enable.should == true
+      @client.should_receive(:enableTable).with @table.table_name
+      @table.enable
     end
   end
   
@@ -31,6 +28,60 @@ describe BlurTable do
       @table.hosts.should == JSON.parse( @table.server )
       @table.hosts.keys.each {|host| host.should match  /Host/}
       @table.hosts.values.flatten.each {|shard| shard.should match /shard/}
+    end
+
+    it "sorts the columns and column families alphabetically" do
+      @unsorted_table = Factory.create :blur_table,
+        :table_name => 'test_table',
+        :table_schema =>
+          { :table              => 'test-table',
+            :setTable           => true,
+            :setColumnFamilies  => true,
+            :columnFamiliesSize => 3,
+            :columnFamilies     => { 'ColumnFamily2' => %w[Column2B Column2C Column2A],
+                                     'ColumnFamily1' => %w[Column1B Column1C Column1A],
+                                     'ColumnFamily3' => %w[Column3B Column3C Column3A] }
+          }.to_json
+      @sorted_table = Factory.create :blur_table,
+        :table_name => 'test-table',
+        :table_schema =>
+          { :table              => 'test-table',
+            :setTable           => true,
+            :setColumnFamilies  => true,
+            :columnFamiliesSize => 3,
+            :columnFamilies     => { 'ColumnFamily1' => %w[Column1A Column1B Column1C],
+                                     'ColumnFamily2' => %w[Column2A Column2B Column2C],
+                                     'ColumnFamily3' => %w[Column3A Column3B Column3C] }
+          }.to_json
+
+        @unsorted_table.schema.should == @sorted_table.schema
+    end
+
+    it "sorts the column families by an optionally supplied block" do
+      @unsorted_table = Factory.create :blur_table,
+        :table_name => 'test_table',
+        :table_schema =>
+          { :table              => 'test-table',
+            :setTable           => true,
+            :setColumnFamilies  => true,
+            :columnFamiliesSize => 3,
+            :columnFamilies     => { 'ColumnFamily1' => %w[Column1B Column1C Column1A],
+                                     'ColumnFamily2' => %w[Column2B Column2C Column2A],
+                                     'ColumnFamily3' => %w[Column3B Column3C Column3A] }
+          }.to_json
+      @reverse_sort_table = Factory.create :blur_table,
+        :table_name => 'test_table',
+        :table_schema =>
+          { :table              => 'test-table',
+            :setTable           => true,
+            :setColumnFamilies  => true,
+            :columnFamiliesSize => 3,
+            :columnFamilies     => { 'ColumnFamily3' => %w[Column3B Column3C Column3A],
+                                     'ColumnFamily2' => %w[Column2B Column2C Column2A],
+                                     'ColumnFamily1' => %w[Column1B Column1C Column1A] }
+          }.to_json
+
+      @unsorted_table.schema.should == @reverse_sort_table.schema {|a, b| b <=> a}
     end
 
     it "returns nil when the server has not been populated" do

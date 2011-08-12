22a52deac52b776cf0b0e4521204bdf08ee67287
Dan Burkert <dburkert@nearinfinity.com>
2011-08-12 10:50:25 -0400

    update blur tables tests

--- blur-admin/spec/integration/blur_tables_spec.rb
+++ blur-admin/spec/integration/blur_tables_spec.rb
@@ -17,22 +17,22 @@ describe "environment status" do
 
   it "displays the table name, number of hosts, shards, records, queries/second, and size in the header" do
     zookeeper.blur_tables.each do |table|
-      find("h3#blur_table_#{table.id}").should have_content table.table_name
-      find("h3#blur_table_#{table.id}").should have_content table.hosts.keys.length
-      find("h3#blur_table_#{table.id}").should have_content table.hosts.values.flatten.length
-      find("h3#blur_table_#{table.id}").should have_content table.query_usage
-      find("h3#blur_table_#{table.id}").should have_content table.is_enabled? ? "Enabled" : "Disabled"
+      find("#blur_table_#{table.id} > h3").should have_content table.table_name
+      find("#blur_table_#{table.id} > h3").should have_content table.hosts.keys.length
+      find("#blur_table_#{table.id} > h3").should have_content table.hosts.values.flatten.length
+      find("#blur_table_#{table.id} > h3").should have_content table.query_usage
+      find("#blur_table_#{table.id} > h3").should have_content table.is_enabled? ? "Enabled" : "Disabled"
     end
   end
 
   it "displays the location and any relevent action buttons in the body" do
     zookeeper.blur_tables.each do |table|
-      find("div#blur_table_#{table.id}").should have_content table.table_uri
+      find("#blur_table_#{table.id}").should have_content table.table_uri
       if table.is_enabled?
-        find("div#blur_table_#{table.id}").should have_selector "input[value='Disable Table']"
+        find("#blur_table_#{table.id}").should have_selector "input[value='Disable Table']"
       else
-        find("div#blur_table_#{table.id}").should have_selector "input[value='Enable Table']"
-        find("div#blur_table_#{table.id}").should have_selector "input[value='Delete Table']"
+        find("#blur_table_#{table.id}").should have_selector "input[value='Enable Table']"
+        find("#blur_table_#{table.id}").should have_selector "input[value='Delete Table']"
       end
     end
   end

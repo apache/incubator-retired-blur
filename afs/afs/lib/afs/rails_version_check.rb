883e6902d3ee2d2a855488624ce596dfb65ceee6
Chris Rohr <crohr@nearinfinity.com>
2011-10-01 20:29:32 -0400

    Some clean up

--- afs/afs/lib/afs/rails_version_check.rb
+++
@@ -1,6 +0,0 @@
-unless Rails.version >= '3.0'
-  msg = []
-  msg << "It seems you are not using Rails 3."
-  msg << "Rails 3 is required to use this plugin."
-  raise msg.join(' ')
-end
--- afs/afs/lib/afs/version.rb
+++
@@ -1,3 +0,0 @@
-module Afs
-  VERSION = '0.1.0'
-end

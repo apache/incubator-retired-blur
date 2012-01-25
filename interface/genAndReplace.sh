78ec6ecd63490c83be5e31217bda498332954d59
Aaron McCurry <amccurry@gmail.com>
2012-01-24 19:29:18 -0500

    Added auto gen of html content.

--- interface/genAndReplace.sh
+++ interface/genAndReplace.sh
@@ -1,4 +1,4 @@
 rm ../src/blur-thrift/src/main/java/com/nearinfinity/blur/thrift/generated/*
-rm -r gen-java/ gen-perl/ gen-rb/
-thrift --gen perl --gen java --gen rb Blur.thrift
+rm -r gen-java/ gen-perl/ gen-rb/ gen-html/
+thrift --gen html --gen perl --gen java --gen rb Blur.thrift
 cp -r gen-java/* ../src/blur-thrift/src/main/java/

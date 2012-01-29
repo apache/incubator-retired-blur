45042524d5e619c3984b9da638f4bd1dbe8b9109
aavenoso <aavenoso@nearinfinity.com>
2012-01-29 11:59:29 -0500

    stop using ganapati

--- blur-admin/lib/hdfs_thrift_client.rb
+++ blur-admin/lib/hdfs_thrift_client.rb
@@ -1,11 +1,68 @@
-require 'ganapati'
 class HdfsThriftClient
   @@connections = {}
 
-  def self.client(host, port)
-    #need to do some stuff with time invalidation
-    url = "#{host}:#{port}"
-    #@@connections[url] ||= ThriftClient.new Ganapati::Client, url, :retries => 10
-    Ganapati::Client.new host, port, nil
+  def self.client(host_port)
+#    if host_port.class == String
+#      urls = host_port.split(/,/)
+#    else
+#      urls = Array(host_port)
+#    end
+#    blacklist_timeout = urls.size
+#    blacklist_timeout = -1 if blacklist_timeout < 2
+#    @@connections[urls] ||= ThriftClient.new ThriftHadoopFileSystem::Client, urls, :server_retry_period=> blacklist_timeout, :retries => 10
+#    Client.new(@@connections[urls])
+#
+    socket = Thrift::Socket.new(host_port.split(":").first,host_port.split(":").last)
+    transport = Thrift::BufferedTransport.new(socket)
+    transport.open
+    protocol = Thrift::BinaryProtocol.new(transport)
+    client = ThriftHadoopFileSystem::Client.new(protocol)
+    
+    Client.new(client)
+  end
+
+  class Client
+    def initialize(client)
+      @client = client
+    end
+
+    def stat(path)
+      @client.stat(pathname(path))
+    end
+
+    def mkdirs(path)
+      @client.mkdirs(pathname(path))
+    end
+
+    def rename(from, to)
+      @client.rename(pathname(from),pathname(to))
+    end
+
+    def delete(path, recursive=false)
+      @client.rm pathname(path), recursive
+    end
+
+    def put(local, remote)
+      handle = @client.create(pathname(remote))
+      Kernel.open(local) { |source|
+        # read 1 MB at a time
+        while record = source.read(1048576)
+          @client.write(handle, record)
+        end
+        @client.close(handle)
+      }
+    end
+
+    def ls(path, recursive=false)
+      Rails.logger.fatal("in ls with #{path}")
+      statuses = @client.listStatus(pathname(path))
+      return statuses unless recursive
+      statuses + statuses.select { |s| s.isdir }.map { |s| ls(s.path, recursive) }.flatten
+    end
+    
+  private
+    def pathname(path)
+      ThriftHadoopFileSystem::Pathname.new(:pathname => path.to_s)
+    end
   end
 end

a915208cf5e17c6826d12ad5a07cd82b321eba93
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-02-10 19:58:12 -0500

    the app is workingish but the asset pipeline has changed pathing and the new json layout is breaking pieces of the app

--- blur-admin/config/environment.rb
+++ blur-admin/config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 BlurAdmin::Application.initialize!
 
-Js::Routes.generate!
+JsRoutes.generate!
 
 BlurAdmin::Application.configure do
   config.gem "authlogic"
--- blur-admin/config/environments/test.rb
+++ blur-admin/config/environments/test.rb
@@ -32,4 +32,5 @@ BlurAdmin::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  config.active_record.mass_assignment_sanitizer = :strict
 end

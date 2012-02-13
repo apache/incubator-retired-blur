e01e1a344eec52b049ecc064459669616b19a07c
Aaron McCurry <amccurry@gmail.com>
2012-02-12 21:22:12 -0500

    Fixed massive memory leak that occured during the caching of search results.

--- src/blur-core/src/main/java/com/nearinfinity/blur/utils/QueryCache.java
+++ src/blur-core/src/main/java/com/nearinfinity/blur/utils/QueryCache.java
@@ -2,21 +2,32 @@ package com.nearinfinity.blur.utils;
 
 import java.util.SortedSet;
 
+import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
+import com.googlecode.concurrentlinkedhashmap.EvictionListener;
 import com.nearinfinity.blur.log.Log;
 import com.nearinfinity.blur.log.LogFactory;
 import com.nearinfinity.blur.thrift.generated.BlurQuery;
 import com.nearinfinity.blur.thrift.generated.BlurResults;
 
-public class QueryCache extends SimpleLRUCache<QueryCacheKey, QueryCacheEntry> {
+public class QueryCache implements EvictionListener<QueryCacheKey, QueryCacheEntry> {
 
   private static final Log LOG = LogFactory.getLog(QueryCache.class);
 
-  private static final long serialVersionUID = -7314843147288776095L;
   private long _ttl;
+  private String _name;
+  private int _cachedElements;
+  private ConcurrentLinkedHashMap<QueryCacheKey, QueryCacheEntry> _cache;
 
   public QueryCache(String name, int cachedElements, long ttl) {
-    super(name, cachedElements);
+    _name = name;
+    _cachedElements = cachedElements;
     _ttl = ttl;
+    _cache = new ConcurrentLinkedHashMap.Builder<QueryCacheKey, QueryCacheEntry>().maximumWeightedCapacity(_cachedElements).listener(this).build();
+  }
+
+  @Override
+  public void onEviction(QueryCacheKey key, QueryCacheEntry value) {
+    LOG.debug("Cache [" + _name + "] key [" + key + "] value [" + value + "] evicted.");
   }
 
   public static QueryCacheKey getNormalizedBlurQueryKey(String table, BlurQuery blurQuery) {
@@ -28,7 +39,7 @@ public class QueryCache extends SimpleLRUCache<QueryCacheKey, QueryCacheEntry> {
     newBlurQuery.uuid = 0;
     newBlurQuery.startTime = 0;
     newBlurQuery.modifyFileCaches = false;
-    return new QueryCacheKey(table,newBlurQuery);
+    return new QueryCacheKey(table, newBlurQuery);
   }
 
   public boolean isValid(QueryCacheEntry entry, SortedSet<String> currentShards) {
@@ -40,7 +51,7 @@ public class QueryCache extends SimpleLRUCache<QueryCacheKey, QueryCacheEntry> {
     }
     return true;
   }
-  
+
   public boolean isValid(QueryCacheEntry entry) {
     if (entry == null) {
       return false;
@@ -59,8 +70,20 @@ public class QueryCache extends SimpleLRUCache<QueryCacheKey, QueryCacheEntry> {
       LOG.debug("Caching results for query [{0}]", original);
       BlurResults cacheResults = new BlurResults(results);
       cacheResults.query = null;
-      put(getNormalizedBlurQueryKey(table,original), new QueryCacheEntry(cacheResults));
+      put(getNormalizedBlurQueryKey(table, original), new QueryCacheEntry(cacheResults));
     }
     return results;
+  }
+
+  public void put(QueryCacheKey key, QueryCacheEntry value) {
+    _cache.put(key, value);
+  }
+
+  public QueryCacheEntry get(QueryCacheKey key) {
+    return _cache.get(key);
+  }
+
+  public void remove(QueryCacheKey key) {
+    _cache.remove(key);
   }
 }

11722e4468a6337d8500dcc62514c30c9a9234de
Ben Kelly <ben@wanderview.com>
2012-02-20 21:53:56 -0500

    (Fix #139) Refactor common code out of BlurIndexReader and BlurIndexWriter into a shared base class.

--- src/blur-core/src/main/java/com/nearinfinity/blur/manager/writer/BlurIndexWriter.java
+++ src/blur-core/src/main/java/com/nearinfinity/blur/manager/writer/BlurIndexWriter.java
@@ -16,11 +16,9 @@
 
 package com.nearinfinity.blur.manager.writer;
 
-import static com.nearinfinity.blur.lucene.LuceneConstant.LUCENE_VERSION;
 import static com.nearinfinity.blur.utils.BlurConstants.ROW_ID;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -28,24 +26,16 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.lucene.index.CorruptIndexException;
-import org.apache.lucene.index.IndexDeletionPolicy;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.KeepOnlyLastCommitDeletionPolicy;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.index.TieredMergePolicy;
-import org.apache.lucene.index.IndexReader.FieldOption;
-import org.apache.lucene.search.Similarity;
 import org.apache.lucene.store.AlreadyClosedException;
 import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.store.IndexOutput;
 import org.apache.lucene.store.LockObtainFailedException;
 
-import com.nearinfinity.blur.analysis.BlurAnalyzer;
 import com.nearinfinity.blur.concurrent.Executors;
 import com.nearinfinity.blur.index.DirectIODirectory;
 import com.nearinfinity.blur.index.WalIndexWriter;
@@ -53,66 +43,38 @@ import com.nearinfinity.blur.index.WalIndexWriter.WalInputFactory;
 import com.nearinfinity.blur.index.WalIndexWriter.WalOutputFactory;
 import com.nearinfinity.blur.log.Log;
 import com.nearinfinity.blur.log.LogFactory;
-import com.nearinfinity.blur.manager.clusterstatus.ClusterStatus;
 import com.nearinfinity.blur.metrics.BlurMetrics;
 import com.nearinfinity.blur.thrift.generated.Row;
-import com.nearinfinity.blur.utils.BlurConstants;
 import com.nearinfinity.blur.utils.RowWalIndexWriter;
-public class BlurIndexWriter extends BlurIndex {
+
+public class BlurIndexWriter extends AbstractBlurIndex {
 
   private static final Log LOG = LogFactory.getLog(BlurIndexWriter.class);
 
-  private DirectIODirectory _directory;
   private WalIndexWriter _writer;
-  private BlurAnalyzer _analyzer;
-  private AtomicReference<IndexReader> _indexReaderRef = new AtomicReference<IndexReader>();
-  private BlurIndexCloser _closer;
-  private BlurIndexRefresher _refresher;
   private RowWalIndexWriter _rowIndexWriter;
   private BlurIndexCommiter _commiter;
-  private AtomicBoolean _open = new AtomicBoolean();
   private String _id = UUID.randomUUID().toString();
   private BlurMetrics _blurMetrics;
-  private String _table;
-  private String _shard;
-  private AtomicBoolean _isClosed = new AtomicBoolean(false);
-  private IndexDeletionPolicy _indexDeletionPolicy = new KeepOnlyLastCommitDeletionPolicy();
-  private Similarity _similarity;
-  private ClusterStatus _clusterStatus;
 
   public void init() throws IOException {
-    IndexWriterConfig conf = new IndexWriterConfig(LUCENE_VERSION, _analyzer);
-    conf.setWriteLockTimeout(TimeUnit.MINUTES.toMillis(5));
-    conf.setIndexDeletionPolicy(_indexDeletionPolicy);
-    conf.setSimilarity(_similarity);
-    TieredMergePolicy mergePolicy = (TieredMergePolicy) conf.getMergePolicy();
-    mergePolicy.setUseCompoundFile(false);
-    _open.set(true);
+    IndexWriterConfig conf = initIndexWriterConfig();
     _writer = openWriter(conf);
     _writer.commitAndRollWal();
-    _indexReaderRef.set(updateSchema(IndexReader.open(_writer, true)));
     _rowIndexWriter = new RowWalIndexWriter(_writer, _analyzer);
     _commiter.addWriter(_id, _writer);
-    _refresher.register(this);
-  }
-
-  private IndexReader updateSchema(IndexReader reader) {
-    if (_clusterStatus != null) {
-      Collection<String> fieldNames = reader.getFieldNames(FieldOption.ALL);
-      _clusterStatus.writeCacheFieldsForTable(BlurConstants.BLUR_CLUSTER,_table,fieldNames);
-    }
-    return reader;
+    initIndexReader(IndexReader.open(_writer, true));
   }
 
   private WalIndexWriter openWriter(final IndexWriterConfig conf) throws CorruptIndexException, LockObtainFailedException, IOException {
-    ExecutorService service = Executors.newSingleThreadExecutor("Writer-Opener-" + _table + "-" + _shard + "-");
+    ExecutorService service = Executors.newSingleThreadExecutor("Writer-Opener-" + getTable() + "-" + getShard() + "-");
     try {
       ExecutorCompletionService<WalIndexWriter> completionService = new ExecutorCompletionService<WalIndexWriter>(service);
       completionService.submit(new Callable<WalIndexWriter>() {
         @Override
         public WalIndexWriter call() throws Exception {
           try {
-            return new WalIndexWriter(_directory, conf, _blurMetrics, new WalOutputFactory() {
+            return new WalIndexWriter(getDirectory(), conf, _blurMetrics, new WalOutputFactory() {
               @Override
               public IndexOutput getWalOutput(DirectIODirectory directory, String name) throws IOException {
                 return directory.createOutputDirectIO(name);
@@ -124,7 +86,7 @@ public class BlurIndexWriter extends BlurIndex {
               }
             });
           } catch (Exception e) {
-            LOG.error("Error trying to open table [{0}] shard [{1}]", e, _table, _shard);
+            LOG.error("Error trying to open table [{0}] shard [{1}]", e, getTable(), getShard());
             throw e;
           }
         }
@@ -138,7 +100,7 @@ public class BlurIndexWriter extends BlurIndex {
           throw new RuntimeException(e);
         }
         if (future == null) {
-          LOG.info("Still trying to open shard for writing table [{0}] shard [{1}]", _table, _shard);
+          LOG.info("Still trying to open shard for writing table [{0}] shard [{1}]", getTable(), getShard());
         } else {
           try {
             return future.get();
@@ -158,7 +120,7 @@ public class BlurIndexWriter extends BlurIndex {
           }
         }
       }
-      throw new IOException("Table [" + _table + "] shard [" + _shard + "] not opened");
+      throw new IOException("Table [" + getTable() + "] shard [" + getShard() + "] not opened");
     } finally {
       service.shutdownNow();
     }
@@ -167,19 +129,8 @@ public class BlurIndexWriter extends BlurIndex {
   @Override
   public void refresh() throws IOException {
     synchronized (_writer) {
-      if (!_open.get()) {
-        return;
-      }
-      IndexReader oldReader = _indexReaderRef.get();
-      if (oldReader.isCurrent()) {
-        return;
-      }
       try {
-        IndexReader reader = IndexReader.openIfChanged(oldReader, _writer, true);
-        if (reader != null && oldReader != reader) {
-          _indexReaderRef.set(updateSchema(reader));
-          _closer.close(oldReader);
-        }
+        super.refresh();
       } catch (AlreadyClosedException e) {
         LOG.warn("Writer was already closed, this can happen during closing of a writer.");
       }
@@ -187,23 +138,15 @@ public class BlurIndexWriter extends BlurIndex {
   }
 
   @Override
-  public IndexReader getIndexReader(boolean forceRefresh) throws IOException {
-    if (forceRefresh) {
-      refresh();
-    }
-    IndexReader indexReader = _indexReaderRef.get();
-    indexReader.incRef();
-    return indexReader;
-  }
-
-  @Override
   public void close() throws IOException {
-    _commiter.remove(_id);
-    _open.set(false);
-    _refresher.unregister(this);
-    _writer.close();
-    _isClosed.set(true);
-    LOG.info("Writer for table [{0}] shard [{1}] closed.",_table,_shard);
+    super.close(new Callable<Void>() {
+      public Void call() throws Exception {
+        _commiter.remove(_id);
+        _writer.close();
+        return null;
+      }
+    });
+    LOG.info("Writer for table [{0}] shard [{1}] closed.",getTable(),getShard());
   }
 
   @Override
@@ -221,22 +164,6 @@ public class BlurIndexWriter extends BlurIndex {
     }
   }
 
-  public void setAnalyzer(BlurAnalyzer analyzer) {
-    _analyzer = analyzer;
-  }
-
-  public void setDirectory(DirectIODirectory directory) {
-    _directory = directory;
-  }
-
-  public void setCloser(BlurIndexCloser closer) {
-    _closer = closer;
-  }
-
-  public void setRefresher(BlurIndexRefresher refresher) {
-    _refresher = refresher;
-  }
-
   public void setCommiter(BlurIndexCommiter commiter) {
     _commiter = commiter;
   }
@@ -245,34 +172,8 @@ public class BlurIndexWriter extends BlurIndex {
     _blurMetrics = blurMetrics;
   }
 
-  public void setTable(String table) {
-    this._table = table;
-  }
-
-  public void setShard(String shard) {
-    this._shard = shard;
-  }
-
-  @Override
-  public AtomicBoolean isClosed() {
-    return _isClosed;
-  }
-
-  public void setIndexDeletionPolicy(IndexDeletionPolicy indexDeletionPolicy) {
-    _indexDeletionPolicy = indexDeletionPolicy;
-  }
-
-  public void setSimilarity(Similarity similarity) {
-    _similarity = similarity;
-  }
-
-  public void setClusterStatus(ClusterStatus clusterStatus) {
-    _clusterStatus = clusterStatus;
-  }
-
   @Override
   public void optimize(int numberOfSegmentsPerShard) throws IOException {
     _writer.forceMerge(numberOfSegmentsPerShard, false);    
   }
-  
 }

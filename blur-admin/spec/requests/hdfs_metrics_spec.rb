9c6cdde33fe5b6837355554df2ec006caca17989
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-02-23 13:25:05 -0500

    removed some of the auto generated tests

--- blur-admin/spec/requests/hdfs_metrics_spec.rb
+++ blur-admin/spec/requests/hdfs_metrics_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe "HdfsMetrics" do
   describe "GET /hdfs_metrics" do
     it "works! (now write some real specs)" do
+    	pending "Just wanted the example"
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get hdfs_metrics_path
-      response.status.should be(200)
+      # get hdfs_metrics_path
+      # response.status.should be(200)
     end
   end
 end

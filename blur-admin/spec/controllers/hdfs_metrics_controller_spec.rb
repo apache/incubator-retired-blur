3e858269ca5f489a4dc26c2b447ddc2f3a32e5d2
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-02-27 10:30:49 -0500

    finished testing metrics, altered the factory slightly, bug fixes found with tests

--- blur-admin/spec/controllers/hdfs_metrics_controller_spec.rb
+++ blur-admin/spec/controllers/hdfs_metrics_controller_spec.rb
@@ -27,7 +27,74 @@ describe HdfsMetricsController do
 
       it "put disk with only id should return all within last minute" do
         put :disk_cap_usage, :id => @hdfs.id
-        assigns(:results).should == @hdfs.hdfs_stats.where("created_at > '#{1.minute.ago}'")
+        assigns(:results).length.should == 1
+      end
+
+      it "put disk with only return the correct properties" do
+        put :disk_cap_usage, :id => @hdfs.id
+        assigns(:results)[0].attribute_names.should == ['id', 'created_at', 'present_capacity', 'dfs_used']
+      end
+
+      it "put disk with stat_mins = 2 should return all within last 2 minutes" do
+        put :disk_cap_usage, :id => @hdfs.id, :stat_min => 2
+        assigns(:results).length.should == 2
+      end
+
+      it "put disk with stat_id = @hdfs.hdfs_stats[1].id should return the last one" do
+        put :disk_cap_usage, :id => @hdfs.id, :stat_id => @hdfs.hdfs_stats[1].id
+        assigns(:results).length.should == 1
+      end
+    end
+
+    describe "PUT live_dead_nodes" do
+      before(:each) do 
+        @hdfs = FactoryGirl.create :hdfs_with_stats
+      end
+
+      it "put nodes with only id should return all within last minute" do
+        put :live_dead_nodes, :id => @hdfs.id
+        assigns(:results).length.should == 1
+      end
+
+      it "put disk with only return the correct properties" do
+        put :live_dead_nodes, :id => @hdfs.id
+        assigns(:results)[0].attribute_names.should == ['id', 'created_at', 'live_nodes', 'dead_nodes']
+      end
+
+      it "put disk with stat_mins = 2 should return all within last 2 minutes" do
+        put :live_dead_nodes, :id => @hdfs.id, :stat_min => 2
+        assigns(:results).length.should == 2
+      end
+
+      it "put disk with stat_id = @hdfs.hdfs_stats[1].id should return the last one" do
+        put :live_dead_nodes, :id => @hdfs.id, :stat_id => @hdfs.hdfs_stats[1].id
+        assigns(:results).length.should == 1
+      end
+    end
+
+    describe "PUT block_info" do
+      before(:each) do 
+        @hdfs = FactoryGirl.create :hdfs_with_stats
+      end
+
+      it "put nodes with only id should return all within last minute" do
+        put :block_info, :id => @hdfs.id
+        assigns(:results).length.should == 1
+      end
+
+      it "put disk with only return the correct properties" do
+        put :block_info, :id => @hdfs.id
+        assigns(:results)[0].attribute_names.should == ['id', 'created_at', 'under_replicated', 'corrupt_blocks']
+      end
+
+      it "put disk with stat_mins = 2 should return all within last 2 minutes" do
+        put :block_info, :id => @hdfs.id, :stat_min => 2
+        assigns(:results).length.should == 2
+      end
+
+      it "put disk with stat_id = @hdfs.hdfs_stats[1].id should return the last one" do
+        put :block_info, :id => @hdfs.id, :stat_id => @hdfs.hdfs_stats[1].id
+        assigns(:results).length.should == 1
       end
     end
   end
--- blur-admin/spec/factories/hdfs_stat.rb
+++ blur-admin/spec/factories/hdfs_stat.rb
@@ -10,6 +10,6 @@ FactoryGirl.define do
     missing_blocks        { rand 5 } 
     total_nodes           { rand 5 }
     dead_nodes            { rand 5 }
-    sequence(:created_at, 0) {|n| n.minute.ago }
+    sequence(:created_at, 0) {|n| ((n % 3) + 0.9).minute.ago }
   end
 end

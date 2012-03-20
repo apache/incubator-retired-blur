47c586397fd21b344f621e82627087c205a4b6ab
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-03-20 18:05:02 -0400

    fixed broken tests

--- blur-admin/spec/controllers/hdfs_metrics_controller_spec.rb
+++ blur-admin/spec/controllers/hdfs_metrics_controller_spec.rb
@@ -18,91 +18,31 @@ describe HdfsMetricsController do
       end
     end
 
-    describe "PUT disk_cap_usage" do
+    describe "PUT stats" do
       before(:each) do 
         @hdfs = FactoryGirl.create :hdfs_with_stats
       end
 
-      it "put disk with only id should return all within last minute" do
-        put :disk_cap_usage, :id => @hdfs.id
+      it "with only id should return all within last minute" do
+        put :stats, :id => @hdfs.id
         assigns(:results).length.should == 1
         response.content_type.should == 'application/json'
       end
 
-      it "put disk with only return the correct properties" do
-        put :disk_cap_usage, :id => @hdfs.id
-        assigns(:results)[0].attribute_names.should == ['id', 'created_at', 'present_capacity', 'dfs_used']
+      it "with only return the correct properties" do
+        put :stats, :id => @hdfs.id
+        assigns(:results)[0].attribute_names.should == %w[id created_at present_capacity dfs_used live_nodes dead_nodes under_replicated corrupt_blocks]
         response.content_type.should == 'application/json'
       end
 
-      it "put disk with stat_mins = 2 should return all within last 2 minutes" do
-        put :disk_cap_usage, :id => @hdfs.id, :stat_mins => 2
+      it "with stat_mins = 2 should return all within last 2 minutes" do
+        put :stats, :id => @hdfs.id, :stat_mins => 2
         assigns(:results).length.should == 2
         response.content_type.should == 'application/json'
       end
 
-      it "put disk with stat_id = @hdfs.hdfs_stats[1].id should return the last one" do
-        put :disk_cap_usage, :id => @hdfs.id, :stat_id => @hdfs.hdfs_stats[1].id
-        assigns(:results).length.should == 1
-        response.content_type.should == 'application/json'
-      end
-    end
-
-    describe "PUT live_dead_nodes" do
-      before(:each) do 
-        @hdfs = FactoryGirl.create :hdfs_with_stats
-      end
-
-      it "put nodes with only id should return all within last minute" do
-        put :live_dead_nodes, :id => @hdfs.id
-        assigns(:results).length.should == 1
-        response.content_type.should == 'application/json'
-      end
-
-      it "put disk with only return the correct properties" do
-        put :live_dead_nodes, :id => @hdfs.id
-        assigns(:results)[0].attribute_names.should == ['id', 'created_at', 'live_nodes', 'dead_nodes']
-        response.content_type.should == 'application/json'
-      end
-
-      it "put disk with stat_mins = 2 should return all within last 2 minutes" do
-        put :live_dead_nodes, :id => @hdfs.id, :stat_mins => 2
-        assigns(:results).length.should == 2
-        response.content_type.should == 'application/json'
-      end
-
-      it "put disk with stat_id = @hdfs.hdfs_stats[1].id should return the last one" do
-        put :live_dead_nodes, :id => @hdfs.id, :stat_id => @hdfs.hdfs_stats[1].id
-        assigns(:results).length.should == 1
-        response.content_type.should == 'application/json'
-      end
-    end
-
-    describe "PUT block_info" do
-      before(:each) do 
-        @hdfs = FactoryGirl.create :hdfs_with_stats
-      end
-
-      it "put nodes with only id should return all within last minute" do
-        put :block_info, :id => @hdfs.id
-        assigns(:results).length.should == 1
-        response.content_type.should == 'application/json'
-      end
-
-      it "put disk with only return the correct properties" do
-        put :block_info, :id => @hdfs.id
-        assigns(:results)[0].attribute_names.should == ['id', 'created_at', 'under_replicated', 'corrupt_blocks']
-        response.content_type.should == 'application/json'
-      end
-
-      it "put disk with stat_mins = 2 should return all within last 2 minutes" do
-        put :block_info, :id => @hdfs.id, :stat_mins => 2
-        assigns(:results).length.should == 2
-        response.content_type.should == 'application/json'
-      end
-
-      it "put disk with stat_id = @hdfs.hdfs_stats[1].id should return the last one" do
-        put :block_info, :id => @hdfs.id, :stat_id => @hdfs.hdfs_stats[1].id
+      it "with stat_id = @hdfs.hdfs_stats[1].id should return the last one" do
+        put :stats, :id => @hdfs.id, :stat_id => @hdfs.hdfs_stats[1].id
         assigns(:results).length.should == 1
         response.content_type.should == 'application/json'
       end
--- blur-admin/spec/routing/hdfs_metrics_routing_spec.rb
+++ blur-admin/spec/routing/hdfs_metrics_routing_spec.rb
@@ -6,16 +6,8 @@ describe HdfsMetricsController do
       get("/hdfs_metrics").should route_to(:controller => "hdfs_metrics", :action => "index")
     end
 
-    it "disk routes to #disk_cap_usage" do
-      put("/hdfs_metrics/1/disk").should route_to(:controller => "hdfs_metrics", :action => "disk_cap_usage", :id => "1")
-    end
-
-    it "nodes routes to #live_dead_nodes" do
-      put("/hdfs_metrics/1/nodes").should route_to(:controller => "hdfs_metrics", :action => "live_dead_nodes", :id => "1")
-    end
-
-    it "blocks routes to #block_info" do
-      put("/hdfs_metrics/1/block").should route_to(:controller => "hdfs_metrics", :action => "block_info", :id => "1")
+    it "disk routes to #stats" do
+      put("/hdfs_metrics/1/stats").should route_to(:controller => "hdfs_metrics", :action => "stats", :id => "1")
     end
   end
 end

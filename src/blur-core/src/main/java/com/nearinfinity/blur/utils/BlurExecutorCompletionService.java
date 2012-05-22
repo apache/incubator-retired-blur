3548502991e114c124f580e32505a5bad3266ba4
Aaron McCurry <amccurry@gmail.com>
2012-05-21 21:46:31 -0400

    Fixed #193

--- src/blur-core/src/main/java/com/nearinfinity/blur/utils/BlurExecutorCompletionService.java
+++ src/blur-core/src/main/java/com/nearinfinity/blur/utils/BlurExecutorCompletionService.java
@@ -127,7 +127,7 @@ public class BlurExecutorCompletionService<T> extends ExecutorCompletionService<
     } catch (InterruptedException e) {
       throw new BException("Call interrupted [{0}]", e, Arrays.asList(parameters));
     } catch (ExecutionException e) {
-      throw new BException("Call execution exception [{0}]", e, Arrays.asList(parameters));
+      throw new BException("Call execution exception [{0}]", e.getCause(), Arrays.asList(parameters));
     }
   }
 

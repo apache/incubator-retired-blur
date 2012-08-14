b4be4402be1600eff827efe28e87825dde346659
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-08-14 14:09:37 -0400

    changed the range for audits to include from and to

--- blur-admin/app/controllers/audits_controller.rb
+++ blur-admin/app/controllers/audits_controller.rb
@@ -2,8 +2,9 @@ class AuditsController < ApplicationController
   def index
     # If a range is given then us it to get the recent audits
     # Otherwise use the default (2 days)
-    hours = params[:hours] || 48
-    @audits = Audit.recent hours.to_i
+    from = params[:from] || 48
+    to = params[:to] || 0
+    @audits = Audit.recent from.to_i, to.to_i
 
     respond_to do |format|
       format.html

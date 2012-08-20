fe4ca00250c84df12dd03fb80e0632bbd7ca1a14
Chris Rohr <crohr@nearinfinity.com>
2012-08-20 04:00:02 -0400

    Fixed #28 by creating a migration to update every table to charset utf8 and collation utf8_bin

--- blur-admin/db/schema.rb
+++ blur-admin/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120813203219) do
+ActiveRecord::Schema.define(:version => 20120820073548) do
 
   create_table "audits", :force => true do |t|
     t.integer  "user_id"

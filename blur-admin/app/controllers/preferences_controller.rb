6be20537c960e2b3691f9806f28c8c008e76dd20
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-08-20 16:09:55 -0400

    more generic auth for prefs

--- blur-admin/app/controllers/preferences_controller.rb
+++ blur-admin/app/controllers/preferences_controller.rb
@@ -1,8 +1,9 @@
 class PreferencesController < ApplicationController
+  load_and_authorize_resource
+
   skip_before_filter :zookeepers, :current_zookeeper
   def update
     @preference = Preference.find_by_pref_type_and_user_id params[:pref_type], params[:user_id]
-    authorize! :update, @preference
     @preference.try(:update_attributes, :value => params['value'])
     render :nothing => true
   end

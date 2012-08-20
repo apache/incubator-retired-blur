6b358e354fb92ade04d9bcb20459e4fe1b7a1a56
Chris Rohr <crohr@nearinfinity.com>
2012-08-20 16:53:11 -0400

    Added MiniCluster to start testing the agent.

---
+++ blur-agent/src/test/java/com/nearinfinity/agent/collectors/TableCollectorTest.java
@@ -0,0 +1,24 @@
+package com.nearinfinity.agent.collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.nearinfinity.BlurAgentBaseTestClass;
+import com.nearinfinity.MockDatasource;
+import com.nearinfinity.blur.MiniCluster;
+
+public class TableCollectorTest extends BlurAgentBaseTestClass {
+	private JdbcTemplate jdbc;
+	
+	@Before
+	public void setup() {
+		jdbc = new JdbcTemplate(new MockDatasource());
+	}
+	
+	@Test
+	public void testCollector() {
+		TableCollector.startCollecting(MiniCluster.getControllerConnectionStr(), "test", jdbc);
+	}
+	
+}

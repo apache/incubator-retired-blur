f9702eafc5419670dc2213722499da0d01d99570
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-08-27 15:53:36 -0400

    removed / changed some names of files, started fixing tests

--- blur-admin/spec/factories/controller.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :controller do
-    sequence (:node_name)     { |n| "Test Node ##{n}" }
-    status                    { rand 3 }
-    blur_version              { "1.#{rand 10}.#{rand 10}" }
-  end
-end

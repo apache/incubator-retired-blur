0bb1f02ebe813c470be2bdcfd97b15d3db0bba02
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-08-30 15:48:52 -0400

    added tests for delete prevention

--- blur-admin/spec/controllers/blur_shards_controller_spec.rb
+++ blur-admin/spec/controllers/blur_shards_controller_spec.rb
@@ -29,7 +29,15 @@ describe BlurShardsController do
 
       it "destroys the shard" do
         @blur_shard.should_receive(:destroy)
+        @blur_shard.status = 0
         delete :destroy, :id => @blur_shard.id, :format => :json
+      end
+
+      it "errors when the shard is enabled" do
+        lambda {
+          @blur_shard.status = 1
+          delete :destroy, :id => @blur_shard.id, :format => :json
+        }.should raise_error
       end
 
       it "logs the event when the shard is deleted" do

4b32c8e5f241bd8a4726cc273778f1c4423578dc
Aaron McCurry <amccurry@gmail.com>
2012-09-02 22:18:57 -0400

    Changing license header to Apache for blur-testsuite.

--- src/blur-testsuite/src/main/java/com/nearinfinity/blur/testsuite/RandomSearchTableContinuously.java
+++ src/blur-testsuite/src/main/java/com/nearinfinity/blur/testsuite/RandomSearchTableContinuously.java
@@ -1,5 +1,21 @@
 package com.nearinfinity.blur.testsuite;
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -60,13 +76,13 @@ public class RandomSearchTableContinuously {
         double avgRate = i / avgSeconds;
         double rate = count / seconds;
         double responseTimeAvg = (responseTime / (double) count) / 1000000.0;
-        System.out.println(System.currentTimeMillis() + "," + i + "," + responseTimeAvg + "," + rate + "," + avgRate + "," + resultCount + "," + getCount(client,tableName));
+        System.out.println(System.currentTimeMillis() + "," + i + "," + responseTimeAvg + "," + rate + "," + avgRate + "," + resultCount + "," + getCount(client, tableName));
         s = now;
         responseTime = 0;
         count = 0;
         resultCount = 0;
       }
-      
+
       builder.setLength(0);
       String query = generateQuery(builder, random, sampleOfTerms, numberOfTermsPerQuery);
       final BlurQuery blurQuery = new BlurQuery();
@@ -75,7 +91,7 @@ public class RandomSearchTableContinuously {
       blurQuery.cacheResult = false;
       blurQuery.selector = new Selector();
       long qs = System.nanoTime();
-      
+
       BlurResults results = client.query(tableName, blurQuery);
       long qe = System.nanoTime();
       resultCount += results.totalResults;

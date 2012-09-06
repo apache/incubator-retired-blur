40c8e37fcaf0301b29109b18b4252eee2bfb552b
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-09-06 14:01:33 -0400

    changed scoping of the manager variable

--- blur-agent/src/main/java/com/nearinfinity/agent/collectors/Collector.java
+++ blur-agent/src/main/java/com/nearinfinity/agent/collectors/Collector.java
@@ -6,18 +6,18 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.nearinfinity.agent.types.InstanceManager;
 
 public abstract class Collector {
-	InstanceManager manager;
+  protected InstanceManager manager;
 
-	protected Collector(InstanceManager manager) {
-		this.manager = manager;
-	}
+  protected Collector(InstanceManager manager) {
+    this.manager = manager;
+  }
 
-	protected ZooKeeper getZk() {
-		return manager.getInstance();
-	}
+  protected ZooKeeper getZk() {
+    return manager.getInstance();
+  }
 
-	protected JdbcTemplate getJdbc() {
-		return manager.getJdbc();
-	}
+  protected JdbcTemplate getJdbc() {
+    return manager.getJdbc();
+  }
 
 }

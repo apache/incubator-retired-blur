f873c145391f13be62d11052c8407d1210d2d913
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-09-10 16:14:20 -0400

    re ordered test to avoid a stateful error

--- blur-admin/spec/controllers/blur_shards_controller_spec.rb
+++ blur-admin/spec/controllers/blur_shards_controller_spec.rb
@@ -33,17 +33,17 @@ describe BlurShardsController do
         delete :destroy, :id => @blur_shard.id, :format => :json
       end
 
+      it "logs the event when the shard is deleted" do
+        @blur_shard.stub!(:destroyed?).and_return true
+        Audit.should_receive :log_event
+        delete :destroy, :id => @blur_shard.id, :format => :json
+      end
+
       it "errors when the shard is enabled" do
         lambda {
           @blur_shard.status = 1
           delete :destroy, :id => @blur_shard.id, :format => :json
         }.should raise_error
-      end
-
-      it "logs the event when the shard is deleted" do
-        @blur_shard.stub!(:destroyed?).and_return true
-        Audit.should_receive :log_event
-        delete :destroy, :id => @blur_shard.id, :format => :json
       end
     end
   end

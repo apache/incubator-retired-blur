20c5463e41443ccd5338dc30bfaabda827003a33
Alex Johnson <ajohnson@nearinfinity.com>
2012-09-18 10:05:18 -0400

    Added basic element testing for blur queries page, but capybara can't handle DataTables so that can't be tested yet.

---
+++ blur-admin/spec/requests/blur_queries_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "Blur Queries" do
+  before(:each) do
+    setup_tests
+  end
+  context "Page is loaded" do
+    it "should display the correct page elements" do
+      visit zookeeper_blur_queries_path(@zookeeper.id)
+      page.should have_table('queries-table')
+      page.should have_content('User ID')
+      page.should have_content('Query')
+      page.should have_content('Table Name')
+      page.should have_content('Starting Record')
+      page.should have_content('Time Submitted')
+      page.should have_content('Status')
+      page.should have_content('Actions/Info')
+      page.should have_css('div', :class => 'range_select')
+    end
+  end
+end

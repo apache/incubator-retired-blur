6350673144a3eb9056cd2448b1639b75acaf843c
Tim <twilliams@apache.org>
2012-09-20 18:31:53 -0400

    Allow overrides in the MiniCluster configuration

--- src/blur-core/src/test/java/org/apache/blur/MiniCluster.java
+++ src/blur-core/src/test/java/org/apache/blur/MiniCluster.java
@@ -24,7 +24,9 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -84,7 +86,14 @@ public abstract class MiniCluster {
     startZooKeeper("./tmp");
     startControllers(1);
     startShards(1);
-
+    
+    //Run the controllers/shards on custom ports.  
+    //BlurConfiguration conf = new BlurConfiguration(false);
+    //conf.setInt(BLUR_CONTROLLER_BIND_PORT, 40001);
+    //conf.setInt(BLUR_SHARD_BIND_PORT, 40002);
+    //startControllers(conf, 1);
+    //startShards(conf, 1);
+    
     try {
       Iface client = BlurClient.getClient(getControllerConnectionStr());
       createTable("test", client);
@@ -179,6 +188,15 @@ public abstract class MiniCluster {
     startControllers(configuration, num);
   }
 
+  private static BlurConfiguration getBlurConfiguration(BlurConfiguration overrides) {
+    BlurConfiguration conf = getBlurConfiguration();
+	
+    for(Map.Entry<String, String> over: overrides.getProperties().entrySet()) {
+      conf.set(over.getKey().toString(), over.getValue().toString());
+    }
+    return conf;
+  }
+  
   private static BlurConfiguration getBlurConfiguration() {
     BlurConfiguration configuration;
     try {
@@ -193,16 +211,19 @@ public abstract class MiniCluster {
     configuration.setLong(BLUR_SHARD_SAFEMODEDELAY, 5000);
     configuration.setInt(BLUR_GUI_CONTROLLER_PORT, -1);
     configuration.setInt(BLUR_GUI_SHARD_PORT, -1);
+
     return configuration;
   }
 
   public static void startControllers(BlurConfiguration configuration, int num) {
     StringBuilder builder = new StringBuilder();
+    BlurConfiguration localConf = getBlurConfiguration(configuration);
+    int controllerPort = localConf.getInt(BLUR_CONTROLLER_BIND_PORT, 40010);
     for (int i = 0; i < num; i++) {
       try {
-        ThriftServer server = ThriftBlurControllerServer.createServer(i, configuration);
+        ThriftServer server = ThriftBlurControllerServer.createServer(i, localConf);
         controllers.add(server);
-        Connection connection = new Connection("localhost", 40010 + i);
+        Connection connection = new Connection("localhost", controllerPort + i);
         if (builder.length() != 0) {
           builder.append(',');
         }
@@ -222,6 +243,7 @@ public abstract class MiniCluster {
   }
 
   public static void startShards(final BlurConfiguration configuration, int num) {
+    final BlurConfiguration localConf = getBlurConfiguration(configuration);
     ExecutorService executorService = Executors.newFixedThreadPool(num);
     List<Future<ThriftServer>> futures = new ArrayList<Future<ThriftServer>>();
     for (int i = 0; i < num; i++) {
@@ -229,16 +251,16 @@ public abstract class MiniCluster {
       futures.add(executorService.submit(new Callable<ThriftServer>() {
         @Override
         public ThriftServer call() throws Exception {
-          return ThriftBlurShardServer.createServer(index, configuration);
+          return ThriftBlurShardServer.createServer(index, localConf);
         }
       }));
     }
-
+    int shardPort = localConf.getInt(BLUR_SHARD_BIND_PORT, 40020);
     for (int i = 0; i < num; i++) {
       try {
         ThriftServer server = futures.get(i).get();
         shards.add(server);
-        Connection connection = new Connection("localhost", 40020 + i);
+        Connection connection = new Connection("localhost", shardPort + i);
         startServer(server, connection);
       } catch (Exception e) {
         LOG.error(e);

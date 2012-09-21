361fd8b91ad51dc61916e6d2a0abfb79e264d23c
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-09-21 17:17:17 -0400

    added query pausing to the query cleaner

--- blur-agent/src/main/java/com/nearinfinity/agent/cleaners/QueriesCleaner.java
+++ blur-agent/src/main/java/com/nearinfinity/agent/cleaners/QueriesCleaner.java
@@ -11,10 +11,14 @@ import com.nearinfinity.agent.connections.interfaces.QueryDatabaseInterface;
 
 public class QueriesCleaner implements Runnable {
   private static final Log log = LogFactory.getLog(QueriesCleaner.class);
-  
+
+  // Time in hours (removes TTL hour old queries)
   private final int timeToLive = -2;
+  // Time in minutes
+  private final int runningTimeToLive = -2;
+
   private final QueryDatabaseInterface database;
-  
+
   public QueriesCleaner(final QueryDatabaseInterface database) {
     this.database = database;
   }
@@ -30,9 +34,15 @@ public class QueriesCleaner implements Runnable {
       ttlHoursAgo.setTimeInMillis(now.getTimeInMillis());
       ttlHoursAgo.add(Calendar.HOUR_OF_DAY, timeToLive);
 
+      Calendar ttlMinutesAgo = Calendar.getInstance();
+      ttlMinutesAgo.setTimeInMillis(now.getTimeInMillis());
+      ttlMinutesAgo.add(Calendar.MINUTE, runningTimeToLive);
+
       int deletedQueries = this.database.deleteOldQueries(ttlHoursAgo.getTime());
-      log.info("Removed " + deletedQueries + " queries!");
-    } catch (DataAccessException e){
+      int expiredQueries = this.database.expireOldQueries(ttlHoursAgo.getTime(), now.getTime());
+      log.info("Removed " + deletedQueries + " queries and " + "Expired " + expiredQueries
+          + " queries, in this pass!");
+    } catch (DataAccessException e) {
       log.error("An error occured while deleting queries from the database!", e);
     } catch (Exception e) {
       log.error("An unkown error occured while cleaning up the queries!", e);

737c284ad3a785bddd6b21329ed627be3760301e
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-09-21 17:13:07 -0400

    fixed a major error where exceptions would prevent child processes from being spawned

--- blur-agent/src/main/java/com/nearinfinity/agent/collectors/blur/table/TableCollector.java
+++ blur-agent/src/main/java/com/nearinfinity/agent/collectors/blur/table/TableCollector.java
@@ -5,9 +5,8 @@ import java.util.List;
 import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
+import com.nearinfinity.agent.connections.interfaces.BlurDatabaseInterface;
 import com.nearinfinity.agent.connections.interfaces.TableDatabaseInterface;
-import com.nearinfinity.agent.exceptions.NullReturnedException;
 import com.nearinfinity.agent.exceptions.TableCollisionException;
 import com.nearinfinity.agent.exceptions.TableMissingException;
 import com.nearinfinity.agent.exceptions.ZookeeperNameCollisionException;
@@ -18,69 +17,91 @@ import com.nearinfinity.blur.thrift.generated.TableDescriptor;
 
 public class TableCollector implements Runnable {
   private static final Log log = LogFactory.getLog(TableCollector.class);
-  
-  private final Iface connection;
+
+  private final Iface blurConnection;
   private final String zookeeper;
+  private final BlurDatabaseInterface blurDatabase;
   private final TableDatabaseInterface database;
 
-  public TableCollector(Iface connection, String zookeeperName, TableDatabaseInterface database) {
-    this.connection = connection;
+  public TableCollector(Iface connection, String zookeeperName, BlurDatabaseInterface blurDatabase,
+      TableDatabaseInterface database) {
+    this.blurConnection = connection;
     this.zookeeper = zookeeperName;
+    this.blurDatabase = blurDatabase;
     this.database = database;
   }
 
   @Override
   public void run() {
     try {
-      log.debug("Collecting table info");
+      /* Retrieve the zookeeper id */
+      String zookeeperId;
+      try {
+        zookeeperId = this.blurDatabase.getZookeeperId(this.zookeeper);
+      } catch (ZookeeperNameMissingException e) {
+        log.error(e.getMessage(), e);
+        return;
+      } catch (ZookeeperNameCollisionException e) {
+        log.error(e.getMessage(), e);
+        return;
+      }
 
-      String zookeeperId = this.database.getZookeeperId(this.zookeeper);
-
-      List<Map<String, Object>> clusters = this.database.getClusters(zookeeperId);
-
-      for (Map<String, Object> cluster : clusters) {
+      /* Retrieve the clusters and their info */
+      for (Map<String, Object> cluster : this.blurDatabase.getClusters(zookeeperId)) {
         String clusterName = (String) cluster.get("NAME");
         Integer clusterId = (Integer) cluster.get("ID");
-
-        List<String> tables = connection.tableListByCluster(clusterName);
-
-        if (tables == null) {
-          throw new NullReturnedException("No tables returned with cluster [" + clusterName + "]!");
+        List<String> tables;
+        try {
+          tables = blurConnection.tableListByCluster(clusterName);
+        } catch (Exception e) {
+          log.error("An error occured while trying to retrieve the table list for cluster["
+              + clusterName + "], skipping cluster", e);
+          continue;
         }
 
+        /* Retrieve the tables and their info */
         for (final String tableName : tables) {
-          TableDescriptor descriptor = connection.describe(tableName);
-
-          if (descriptor == null) {
-            log.error("Unable to describe table [" + tableName + "].");
+          TableDescriptor descriptor;
+          try {
+            descriptor = blurConnection.describe(tableName);
+          } catch (Exception e) {
+            log.error("An error occured while trying to describe the table [" + tableName
+                + "], skipping table", e);
             continue;
           }
 
-          Map<String, Object> existingTable = this.database.getExistingTable(tableName, clusterId);
+          /* Retrieve the table from the db, if it exists
+           * then update its information */
+          Map<String, Object> existingTable;
+          try {
+            existingTable = this.blurDatabase.getExistingTable(tableName, clusterId);
+          } catch (TableMissingException e) {
+            log.error(e.getMessage(), e);
+            continue;
+          } catch (TableCollisionException e) {
+            log.error(e.getMessage(), e);
+            continue;
+          }
+          
+          /* Insert the table into the global table map */
           Map<String, Object> tableInfo = new HashMap<String, Object>();
           tableInfo.put("ID", existingTable.get("id"));
           tableInfo.put("ENABLED", descriptor.isEnabled);
           TableMap.getInstance().put(tableName + "_" + clusterName, tableInfo);
-
+          
+          /* spawn the different table info collectors */
           if (descriptor.isEnabled) {
-            new Thread(new SchemaCollector(this.connection, tableName, clusterId, descriptor, this.database)).start();
+            new Thread(new SchemaCollector(this.blurConnection, tableName, clusterId, descriptor,
+                this.database)).start();
           }
-          new Thread(new ServerCollector(connection, tableName, clusterId, this.database)).start();
-          new Thread(new StatsCollector(connection, tableName, clusterId, this.database)).start();
+          new Thread(new ServerCollector(this.blurConnection, tableName, clusterId, this.database))
+              .start();
+          new Thread(new StatsCollector(this.blurConnection, tableName, clusterId, this.database))
+              .start();
         }
       }
-    } catch (ZookeeperNameCollisionException e) {
-      log.error(e.getMessage(), e);
-    } catch (ZookeeperNameMissingException e) {
-      log.error(e.getMessage(), e);
-    } catch (TableMissingException e) {
-      log.error(e.getMessage(), e);
-    } catch (TableCollisionException e) {
-      log.error(e.getMessage(), e);
-    } catch (NullReturnedException e) {
-      log.error(e.getMessage(), e);
     } catch (Exception e) {
-      log.error("An unknown error occurred in the CollectorManager.", e);
+      log.error("An unknown error occurred.", e);
     }
   }
 }

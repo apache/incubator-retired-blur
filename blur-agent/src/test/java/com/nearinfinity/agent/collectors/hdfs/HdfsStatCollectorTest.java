d9dacb4f7dd23907665a7a6682ac7a17354b0912
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-11-01 14:16:06 -0400

    hdfs and stat collector tests

---
+++ blur-agent/src/test/java/com/nearinfinity/agent/collectors/hdfs/HdfsStatCollectorTest.java
@@ -0,0 +1,54 @@
+package com.nearinfinity.agent.collectors.hdfs;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.nearinfinity.BlurAgentBaseTestClass;
+import com.nearinfinity.agent.connections.hdfs.HdfsDatabaseConnection;
+import com.nearinfinity.agent.connections.hdfs.interfaces.HdfsDatabaseInterface;
+import com.nearinfinity.blur.MiniCluster;
+
+public class HdfsStatCollectorTest extends BlurAgentBaseTestClass {
+	private static HdfsDatabaseInterface database = new HdfsDatabaseConnection(jdbc);
+	private static URI hdfsUri;
+
+	@Before
+	public void setup() throws IOException {
+		hdfsUri = MiniCluster.getFileSystemUri();
+	}
+
+	@Test
+	public void shouldNotInsertStatsWithoutParent() {
+		Thread testHdfsStatsCollector = new Thread(new HdfsStatsCollector("Test HDFS", hdfsUri, null, database), "Query Test Thread");
+		testHdfsStatsCollector.start();
+		try {
+			testHdfsStatsCollector.join();
+		} catch (InterruptedException e) {
+			fail("The test HdfsStatCollector failed while waiting for it to finish!");
+		}
+
+		int statsCount = jdbc.queryForInt("select count(id) from hdfs_stats");
+		assertEquals(0, statsCount);
+	}
+	
+	@Test
+	public void shouldInsertStats() {
+		jdbc.update("insert into hdfs (name) values (?)", "TestHdfs");
+		Thread testHdfsStatsCollector = new Thread(new HdfsStatsCollector("TestHDFS", hdfsUri, null, database), "Query Test Thread");
+		testHdfsStatsCollector.start();
+		try {
+			testHdfsStatsCollector.join();
+		} catch (InterruptedException e) {
+			fail("The test HdfsStatCollector failed while waiting for it to finish!");
+		}
+
+		int statsCount = jdbc.queryForInt("select count(id) from hdfs_stats");
+		assertEquals(1, statsCount);
+	}
+}

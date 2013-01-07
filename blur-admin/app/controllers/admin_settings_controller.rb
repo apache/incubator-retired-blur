51e89c8a54b4969a14f7d7f0b4b3035fbca5dd93
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2013-01-07 11:32:34 -0500

    added testing for the admin setting controller

--- blur-admin/app/controllers/admin_settings_controller.rb
+++ blur-admin/app/controllers/admin_settings_controller.rb
@@ -2,9 +2,9 @@ class AdminSettingsController < ApplicationController
   respond_to :html
 
   def update
-    @search_filter = AdminSetting.find_or_create_by_setting(params[:setting])
-    @search_filter.value = params[:value]
-    @search_filter.save
+    search_filter = AdminSetting.find_or_create_by_setting(params[:setting])
+    search_filter.value = params[:value]
+    search_filter.save
     respond_with do |format|
       format.html { render :nothing => true }
     end

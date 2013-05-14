c19fa07bbf55c037d8f91b2d4e793afe1d2eb6bf
Aaron McCurry <amccurry@gmail.com>
2013-05-14 12:20:10 -0400

    New util program.

---
+++ src/blur-testsuite/src/main/java/org/apache/blur/testsuite/StatsTable.java
@@ -0,0 +1,38 @@
+package org.apache.blur.testsuite;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.io.IOException;
+
+import org.apache.blur.thrift.BlurClient;
+import org.apache.blur.thrift.generated.BlurException;
+import org.apache.blur.thrift.generated.Blur.Iface;
+import org.apache.blur.thrift.generated.TableStats;
+import org.apache.thrift.TException;
+
+
+public class StatsTable {
+
+  public static void main(String[] args) throws BlurException, TException, IOException {
+    String connectionStr = args[0];
+    final String tableName = args[1];
+
+    Iface client = BlurClient.getClient(connectionStr);
+    TableStats tableStats = client.getTableStats(tableName);
+    System.out.println(tableStats);
+  }
+}

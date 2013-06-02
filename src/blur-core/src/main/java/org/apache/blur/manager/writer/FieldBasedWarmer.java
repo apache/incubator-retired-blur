9a0b732129c2d4867122c9b32088e6d3143c317e
Aaron McCurry <amccurry@gmail.com>
2013-06-02 18:18:01 -0400

    Add license header.

--- src/blur-core/src/main/java/org/apache/blur/manager/writer/FieldBasedWarmer.java
+++ src/blur-core/src/main/java/org/apache/blur/manager/writer/FieldBasedWarmer.java
@@ -1,5 +1,21 @@
 package org.apache.blur.manager.writer;
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.io.IOException;
 import java.util.List;
 
@@ -50,29 +66,29 @@ public class FieldBasedWarmer extends IndexReaderWarmer {
   }
 
   private void warm(AtomicReader reader, Iterable<String> fieldNames) throws IOException {
-//    for (String field : fieldNames) {
-//      LOG.debug("Warming field [{0}] in reader [{1}]", field, reader);
-//      
-//      AtomicReaderContext context = reader.getContext();
-//      context.
-//      
-//      Fields fields = reader.fields();
-//      Terms terms = fields.terms(field);
-//      TermsEnum termsEnum = terms.iterator(null);
-//      BytesRef ref = null;
-//      Term term = new Term(field);
-//      while ((ref = termsEnum.next()) != null) {
-//        term.set(field, ref);
-//        DocsAndPositionsEnum termPositionsEnum = reader.termPositionsEnum(term);
-//        if (termPositionsEnum != null) {
-//          while (termPositionsEnum.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
-//            int freq = termPositionsEnum.freq();
-//            for (int i = 0; i < freq; i++) {
-//              termPositionsEnum.nextPosition();
-//            }
-//          }
-//        }
-//      }
-//    }
+    // for (String field : fieldNames) {
+    // LOG.debug("Warming field [{0}] in reader [{1}]", field, reader);
+    //
+    // AtomicReaderContext context = reader.getContext();
+    // context.
+    //
+    // Fields fields = reader.fields();
+    // Terms terms = fields.terms(field);
+    // TermsEnum termsEnum = terms.iterator(null);
+    // BytesRef ref = null;
+    // Term term = new Term(field);
+    // while ((ref = termsEnum.next()) != null) {
+    // term.set(field, ref);
+    // DocsAndPositionsEnum termPositionsEnum = reader.termPositionsEnum(term);
+    // if (termPositionsEnum != null) {
+    // while (termPositionsEnum.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
+    // int freq = termPositionsEnum.freq();
+    // for (int i = 0; i < freq; i++) {
+    // termPositionsEnum.nextPosition();
+    // }
+    // }
+    // }
+    // }
+    // }
   }
 }

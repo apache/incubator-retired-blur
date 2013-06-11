b12b1a9a31116e6938b59c65330d3d0e736664c2
Aaron McCurry <amccurry@gmail.com>
2013-06-10 22:33:05 -0400

    Starting new test for jdbc driver, there's not code for testing anything yet.

---
+++ blur-jdbc/src/test/java/org/apache/blur/jdbc/BlurJdbcTest.java
@@ -0,0 +1,24 @@
+package org.apache.blur.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+public class BlurJdbcTest {
+  
+  @Test
+  public void testJdbcDriver() throws SQLException, ClassNotFoundException {
+//    Class.forName(BlurJdbc.class.getName());
+//    Connection connection = DriverManager.getConnection("jdbc:blur:blur-vm:40010");
+//    Statement statement = connection.createStatement();
+//    ResultSet resultSet = statement.executeQuery("select * from test8493");
+//    while (resultSet.next()) {
+//      System.out.println(resultSet.getString(1));
+//    }
+  }
+
+}

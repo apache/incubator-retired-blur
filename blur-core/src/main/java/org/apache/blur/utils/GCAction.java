10b2637679e3810d8df9162bb1f9c987b19359b2
Aaron McCurry <amccurry@gmail.com>
2013-10-07 08:56:23 -0400

    In preparation for supporting java 7 and the G1 collector, adding a java 7 enhanced version of the GCWatcher.

--- blur-core/src/main/java/org/apache/blur/utils/GCAction.java
+++ blur-core/src/main/java/org/apache/blur/utils/GCAction.java
@@ -16,36 +16,10 @@
  */
 package org.apache.blur.utils;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-public class CreateGarbage {
-
-  public static void main(String[] args) throws InterruptedException {
-    final Map<String, String> map = new ConcurrentHashMap<String, String>();
-    GCAction action = new GCAction() {
-      @Override
-      public void takeAction() throws Exception {
-        map.clear();
-      }
-    };
-    GCWatcher.init(0.75);
-    GCWatcher.registerAction(action);
-    while (true) {
-
-      int count = 0;
-      int max = 100000;
-      for (long i = 0; i < 100000000; i++) {
-        if (count >= max) {
-          Thread.sleep(250);
-          count = 0;
-        }
-        map.put(Long.toString(i), Long.toString(i));
-        count++;
-      }
-      System.out.println(map.size());
-      map.clear();
-    }
-  }
-
+/**
+ * The {@link GCAction} interface is used to implement an action that needs to
+ * be taken to prevent an {@link OutOfMemoryError} exception.
+ */
+public interface GCAction {
+  void takeAction() throws Exception;
 }
--- blur-core/src/main/java/org/apache/blur/utils/GCWatcher.java
+++ blur-core/src/main/java/org/apache/blur/utils/GCWatcher.java
@@ -1,5 +1,3 @@
-package org.apache.blur.utils;
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,231 +14,67 @@ package org.apache.blur.utils;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
+package org.apache.blur.utils;
 
-import org.apache.blur.log.Log;
-import org.apache.blur.log.LogFactory;
+import java.util.Properties;
 
-public class GCWatcher extends TimerTask {
+public class GCWatcher {
 
-  private static final Log LOG = LogFactory.getLog(GCWatcher.class);
-  private static final String GET_LAST_GC_INFO = "getLastGcInfo";
-  private static final String CONCURRENT_MARK_SWEEP = "ConcurrentMarkSweep";
-  private static final String CMS_OLD_GEN = "CMS Old Gen";
+  private static final String JAVA_VERSION = "java.version";
+  private static final String _1_7 = "1.7";
+  private static final boolean JDK7;
 
-  private final Timer _timer;
-  private final long _period = TimeUnit.MILLISECONDS.toMillis(25);
-  private GarbageCollectorMXBean _bean;
-  private final double _ratio;
-  private Method _method;
-  private GcInfo _gcInfo;
-  private long _lastIndex;
-  private final List<Action> _actions = new ArrayList<Action>();
-  private final MemoryMXBean _memoryMXBean;
-  private static GCWatcher _instance;
-
-  public interface Action {
-    void takeAction() throws Exception;
-  }
-
-  private GCWatcher(double ratio) {
-    _memoryMXBean = ManagementFactory.getMemoryMXBean();
-    List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
-    for (GarbageCollectorMXBean bean : garbageCollectorMXBeans) {
-      String name = bean.getName();
-      if (name.equals(CONCURRENT_MARK_SWEEP)) {
-        _bean = bean;
-        try {
-          _method = _bean.getClass().getDeclaredMethod(GET_LAST_GC_INFO, new Class[] {});
-        } catch (SecurityException e) {
-          throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
-          throw new RuntimeException(e);
-        }
-        _method.setAccessible(true);
-      }
-    }
-    _ratio = ratio;
-    if (_bean != null) {
-      _timer = new Timer("gc-watch", true);
-      _timer.schedule(this, _period, _period);
-      LOG.info("GCWatcher was setup.");
+  static {
+    Properties properties = System.getProperties();
+    String javaVersion = properties.getProperty(JAVA_VERSION);
+    if (javaVersion.startsWith(_1_7)) {
+      JDK7 = true;
     } else {
-      _timer = null;
-      LOG.warn("GCWatcher was NOT setup.");
+      JDK7 = false;
     }
   }
 
-  public static void registerAction(Action action) {
-    GCWatcher instance = instance();
-    if (instance != null) {
-      synchronized (instance._actions) {
-        instance._actions.add(action);
-      }
-    }
-  }
-
-  public static synchronized void shutdown() {
-    GCWatcher instance = instance();
-    if (instance != null) {
-      if (instance._timer != null) {
-        instance._timer.purge();
-        instance._timer.cancel();
-      }
-    }
-  }
-
-  @Override
-  public void run() {
-    try {
-      Object gcInfo = getGcInfo(_bean);
-      if (gcInfo != null) {
-        if (_gcInfo == null) {
-          _gcInfo = new GcInfo(gcInfo);
-        } else {
-          _gcInfo.setGcInfo(gcInfo);
-        }
-        if (_lastIndex == _gcInfo.getIndex()) {
-          return;
-        }
-        long startTime = _gcInfo.getStartTime();
-        long endTime = _gcInfo.getEndTime();
-        Map<String, MemoryUsage> usageBeforeGc = _gcInfo.getUsageBeforeGc();
-        Map<String, MemoryUsage> usageAfterGc = _gcInfo.getUsageAfterGc();
-        
-        MemoryUsage before = usageBeforeGc.get(CMS_OLD_GEN);
-        long usedBefore = before.getUsed();
-        
-        MemoryUsage after = usageAfterGc.get(CMS_OLD_GEN);
-        long usedAfter = after.getUsed();
-        
-        long totalTime = endTime - startTime;
-        LOG.info("totalTime spent in GC [{0} ms] collected [{1} bytes]", totalTime, (usedBefore - usedAfter));
-        MemoryUsage heapMemoryUsage = _memoryMXBean.getHeapMemoryUsage();
-        long max = heapMemoryUsage.getMax();
-        long used = heapMemoryUsage.getUsed();
-        long upperLimit = (long) (max * _ratio);
-        if (used > upperLimit) {
-          LOG.error("----- WARNING !!!! - Heap used [{0}] over limit of [{1}], taking action to avoid an OOM error.", used,
-              upperLimit);
-          synchronized (_actions) {
-            for (Action action : _actions) {
-              try {
-                action.takeAction();
-              } catch (Exception e) {
-                LOG.error("Unknown error while trying to take action against an OOM [{0}]", e, action);
-              }
-            }
-          }
-        }
-        _lastIndex = _gcInfo.getIndex();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private Object getGcInfo(GarbageCollectorMXBean bean) throws Exception {
-    return _method.invoke(bean, new Object[] {});
-  }
-
-  static class GcInfo {
-
-    private Object _gcInfo;
-    private final Field _startTimeField;
-    private final Field _endTimeField;
-    private final Field _usageAfterGcField;
-    private final Field _usageBeforeGcField;
-    private final Field _indexField;
-
-    GcInfo(Object o) throws SecurityException, NoSuchFieldException {
-      Class<? extends Object> clazz = o.getClass();
-      _startTimeField = setup(clazz.getDeclaredField("startTime"));
-      _endTimeField = setup(clazz.getDeclaredField("endTime"));
-      _usageAfterGcField = setup(clazz.getDeclaredField("usageAfterGc"));
-      _usageBeforeGcField = setup(clazz.getDeclaredField("usageBeforeGc"));
-      _indexField = setup(clazz.getDeclaredField("index"));
-      setGcInfo(o);
-    }
-
-    void setGcInfo(Object o) {
-      _gcInfo = o;
-    }
-
-    private Field setup(Field field) {
-      field.setAccessible(true);
-      return field;
-    }
-
-    long getStartTime() {
-      try {
-        return _startTimeField.getLong(_gcInfo);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    long getEndTime() {
-      try {
-        return _endTimeField.getLong(_gcInfo);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    long getIndex() {
-      try {
-        return _indexField.getLong(_gcInfo);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    @SuppressWarnings("unchecked")
-    Map<String, MemoryUsage> getUsageAfterGc() {
-      try {
-        return (Map<String, MemoryUsage>) _usageAfterGcField.get(_gcInfo);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    @SuppressWarnings("unchecked")
-    Map<String, MemoryUsage> getUsageBeforeGc() {
-      try {
-        return (Map<String, MemoryUsage>) _usageBeforeGcField.get(_gcInfo);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-  }
-
-  public synchronized static void init(double ratio) {
-    if (_instance == null) {
-      try {
-        _instance = new GCWatcher(ratio);
-      } catch (Exception e) {
-        LOG.error("GCWatcher had error initializing", e);
-      }
+  /**
+   * Initializes the GCWatcher to watch for any garbage collection that leaves
+   * more then the given ratio free. If more remains then all the given
+   * {@link GCAction}s are taken to try and relief the JVM from an
+   * {@link OutOfMemoryError} exception.
+   * 
+   * @param ratio
+   *          the ratio of used heap to total heap.
+   */
+  public static void init(double ratio) {
+    if (JDK7) {
+      GCWatcherJdk7.init(ratio);
     } else {
-      LOG.warn("GCWatcher has already been initialized");
+      GCWatcherJdk6.init(ratio);
     }
   }
 
-  private static GCWatcher instance() {
-    return _instance;
+  /**
+   * Registers an {@link GCAction} to be taken when the JVM is near an
+   * {@link OutOfMemoryError} condition.
+   * 
+   * @param action
+   *          the {@link GCAction}.
+   */
+  public static void registerAction(GCAction action) {
+    if (JDK7) {
+      GCWatcherJdk7.registerAction(action);
+    } else {
+      GCWatcherJdk6.registerAction(action);
+    }
+  }
+
+  /**
+   * Shuts down any internal threads watching the JVM.
+   */
+  public static void shutdown() {
+    if (JDK7) {
+      GCWatcherJdk7.shutdown();
+    } else {
+      GCWatcherJdk6.shutdown();
+    }
   }
 
 }

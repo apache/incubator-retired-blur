91812704327907bb3e36a40e0eb171eb6b36b0c6
Aaron McCurry <amccurry@gmail.com>
2013-12-06 10:52:27 -0500

    Adding new commands to be able to manage traces from command line.

---
+++ blur-shell/src/main/java/org/apache/blur/shell/TraceRemove.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.blur.shell;
+
+import java.io.PrintWriter;
+
+import org.apache.blur.thirdparty.thrift_0_9_0.TException;
+import org.apache.blur.thrift.generated.Blur;
+import org.apache.blur.thrift.generated.BlurException;
+
+public class TraceRemove extends Command {
+  @Override
+  public void doit(PrintWriter out, Blur.Iface client, String[] args) throws CommandException, TException,
+      BlurException {
+    if (args.length < 2) {
+      throw new CommandException("Invalid args: " + help());
+    }
+    String traceId = args[1];
+    client.traceRemove(traceId);
+  }
+
+  @Override
+  public String description() {
+    return "Delete trace by id.";
+  }
+
+  @Override
+  public String usage() {
+    return "<trace id>";
+  }
+
+  @Override
+  public String name() {
+    return "trace-remove";
+  }
+}

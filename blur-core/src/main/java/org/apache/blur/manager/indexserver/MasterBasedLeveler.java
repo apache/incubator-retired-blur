49daa7c038b695f8df691519ab3f0296c6596536
Aaron McCurry <amccurry@gmail.com>
2013-12-08 15:03:41 -0500

    Improving the leveling logic, and adding a test.

--- blur-core/src/main/java/org/apache/blur/manager/indexserver/MasterBasedLeveler.java
+++ blur-core/src/main/java/org/apache/blur/manager/indexserver/MasterBasedLeveler.java
@@ -24,6 +24,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -35,8 +36,8 @@ public class MasterBasedLeveler {
 
   private static final Log LOG = LogFactory.getLog(MasterBasedLeveler.class);
 
-  public static void level(int totalShards, int totalShardServers, Map<String, Integer> onlineServerShardCount,
-      Map<String, String> newLayoutMap, String table) {
+  public static int level(int totalShards, int totalShardServers, Map<String, Integer> onlineServerShardCount,
+      Map<String, String> newLayoutMap, String table, Random random) {
     List<Entry<String, Integer>> onlineServerShardCountList = new ArrayList<Map.Entry<String, Integer>>(
         onlineServerShardCount.entrySet());
     Collections.sort(onlineServerShardCountList, new Comparator<Entry<String, Integer>>() {
@@ -55,17 +56,17 @@ public class MasterBasedLeveler {
 
     Set<String> overAllocatedSet = new HashSet<String>();
     Set<String> underAllocatedSet = new HashSet<String>();
-    LOG.info("Optimum server shard count [{0}] for table [{1}]", opt, table);
+    LOG.debug("Optimum server shard count [{0}] for table [{1}]", opt, table);
     for (Entry<String, Integer> e : onlineServerShardCountList) {
       int countInt = e.getValue();
       float count = countInt;
       String server = e.getKey();
       if (isNotInOptBalance(opt, count)) {
         if (count > opt) {
-          LOG.info("Level server [{0}] over allocated at [{1}]", server, e.getValue());
+          LOG.debug("Level server [{0}] over allocated at [{1}]", server, e.getValue());
           overAllocatedSet.add(server);
         } else {
-          LOG.info("Level server [{0}] under allocated at [{1}]", server, e.getValue());
+          LOG.debug("Level server [{0}] under allocated at [{1}]", server, e.getValue());
           underAllocatedSet.add(server);
         }
       }
@@ -83,13 +84,56 @@ public class MasterBasedLeveler {
       shards.add(shard);
     }
 
+    int moves = 0;
     while (!underAllocatedSet.isEmpty() && !overAllocatedSet.isEmpty()) {
       String overAllocatedServer = getFirst(overAllocatedSet);
       String underAllocatedServer = getFirst(underAllocatedSet);
-      LOG.info("Over allocated server [{0}] under allocated server [{1}]", overAllocatedServer, underAllocatedServer);
+      LOG.debug("Over allocated server [{0}] under allocated server [{1}]", overAllocatedServer, underAllocatedServer);
       moveSingleShard(overAllocatedServer, underAllocatedServer, opt, overAllocatedSet, underAllocatedSet,
           newLayoutMap, onlineServerShardCount, serverToShards, table);
+      moves++;
     }
+
+    if (overAllocatedSet.size() > 0) {
+      int count = (int) opt;
+      LOG.debug("There are still [{0}] over allocated servers for table [{1}]", overAllocatedSet.size(), table);
+      while (!overAllocatedSet.isEmpty()) {
+        String overAllocatedServer = getFirst(overAllocatedSet);
+        String underAllocatedServer = findServerWithCount(onlineServerShardCount, count, table, random);
+        LOG.debug("Over allocated server [{0}] under allocated server [{1}]", overAllocatedServer, underAllocatedServer);
+        moveSingleShard(overAllocatedServer, underAllocatedServer, opt, overAllocatedSet, underAllocatedSet,
+            newLayoutMap, onlineServerShardCount, serverToShards, table);
+        moves++;
+      }
+    }
+
+    if (underAllocatedSet.size() > 0) {
+      int count = (int) Math.ceil(opt);
+      LOG.info("There are still [{0}] under allocated servers for table [{1}]", underAllocatedSet.size(), table);
+      while (!underAllocatedSet.isEmpty()) {
+        String overAllocatedServer = findServerWithCount(onlineServerShardCount, count, table, random);
+        String underAllocatedServer = getFirst(underAllocatedSet);
+        LOG.debug("Over allocated server [{0}] under allocated server [{1}]", overAllocatedServer, underAllocatedServer);
+        moveSingleShard(overAllocatedServer, underAllocatedServer, opt, overAllocatedSet, underAllocatedSet,
+            newLayoutMap, onlineServerShardCount, serverToShards, table);
+        moves++;
+      }
+    }
+    return moves;
+  }
+
+  private static String findServerWithCount(Map<String, Integer> onlineServerShardCount, int count, String table,
+      Random random) {
+    LOG.debug("Looking for server with shard count [{0}] for table [{1}]", count, table);
+    List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(onlineServerShardCount.entrySet());
+    Collections.shuffle(list, random);
+    for (Entry<String, Integer> e : list) {
+      int serverCount = e.getValue();
+      if (serverCount == count) {
+        return e.getKey();
+      }
+    }
+    throw new RuntimeException("This should never happen");
   }
 
   private static boolean isNotInOptBalance(float opt, float count) {
@@ -115,24 +159,26 @@ public class MasterBasedLeveler {
       serverToShards.put(distServer, distShards);
     }
 
-    LOG.info("Source server shard list for table [{0}] is [{1}]", table, srcShards);
-    LOG.info("Destination server shard list for table [{0}] is [{1}]", table, distShards);
+    LOG.debug("Source server shard list for table [{0}] is [{1}]", table, srcShards);
+    LOG.debug("Destination server shard list for table [{0}] is [{1}]", table, distShards);
 
     String srcShard = getFirst(srcShards);
 
-    LOG.info("Moving shard [{0}] from [{1}] to [{2}] for table [{3}]", srcShard, srcServer, distServer, table);
+    LOG.debug("Moving shard [{0}] from [{1}] to [{2}] for table [{3}]", srcShard, srcServer, distServer, table);
 
     srcShards.remove(srcShard);
     distShards.add(srcShard);
 
     if (!isNotInOptBalance(opt, srcShards.size())) {
-      LOG.info("Source server [{0}] is in balance with size [{1}] optimum size [{2}]", srcServer, srcShards.size(), opt);
+      LOG.debug("Source server [{0}] is in balance with size [{1}] optimum size [{2}]", srcServer, srcShards.size(),
+          opt);
       overAllocatedServerSet.remove(srcServer);
       underAllocatedServerSet.remove(srcServer);
     }
 
     if (!isNotInOptBalance(opt, distShards.size())) {
-      LOG.info("Source server [{0}] is in balance with size [{1}] optimum size [{2}]", distServer, distShards.size(), opt);
+      LOG.debug("Source server [{0}] is in balance with size [{1}] optimum size [{2}]", distServer, distShards.size(),
+          opt);
       overAllocatedServerSet.remove(distServer);
       underAllocatedServerSet.remove(distServer);
     }

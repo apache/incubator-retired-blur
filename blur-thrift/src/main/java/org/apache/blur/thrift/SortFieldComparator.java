2f17851dce28aca67bf78cdc073df04dee094bac
Aaron McCurry <amccurry@gmail.com>
2014-04-29 10:55:35 -0400

    Removing dead code and cleaning up deps.

--- blur-thrift/src/main/java/org/apache/blur/thrift/SortFieldComparator.java
+++ blur-thrift/src/main/java/org/apache/blur/thrift/SortFieldComparator.java
@@ -20,7 +20,6 @@ import java.util.Comparator;
 
 import org.apache.blur.thrift.generated.SortFieldResult;
 import org.apache.blur.thrift.generated.SortFieldResult._Fields;
-import org.apache.hadoop.io.WritableComparator;
 
 public class SortFieldComparator implements Comparator<SortFieldResult> {
 
@@ -53,7 +52,20 @@ public class SortFieldComparator implements Comparator<SortFieldResult> {
   }
 
   public int compare(byte[] b1, byte[] b2) {
-    return WritableComparator.compareBytes(b1, 0, b1.length, b2, 0, b2.length);
+    return compareBytes(b1, 0, b1.length, b2, 0, b2.length);
+  }
+
+  public static int compareBytes(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+    int end1 = s1 + l1;
+    int end2 = s2 + l2;
+    for (int i = s1, j = s2; i < end1 && j < end2; i++, j++) {
+      int a = (b1[i] & 0xff);
+      int b = (b2[j] & 0xff);
+      if (a != b) {
+        return a - b;
+      }
+    }
+    return l1 - l2;
   }
 
 }

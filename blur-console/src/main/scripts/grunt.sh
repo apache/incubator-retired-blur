696bba3c415fe7139c93b0dd9375cba43cafdc24
Chris Rohr <rohr.chris@gmail.com>
2014-07-08 16:15:19 -0400

    Made sure the mvn install and rat checks were good

--- blur-console/src/main/scripts/grunt.sh
+++ blur-console/src/main/scripts/grunt.sh
@@ -11,27 +11,11 @@ DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 # Check for grunt-cli installed: log `npm install -g grunt-cli`
 command -v grunt >/dev/null 2>&1 || { echo >&2 "[ERROR] grunt not found! install: npm install -g grunt-cli"; exit 1; }
 
-# Check for bower: log `npm install -g bower`
-command -v bower >/dev/null 2>&1 || { echo >&2 "[ERROR] bower not found! install: npm install -g bower"; exit 1; }
-
 # run `npm install` in src/main/webapp
-cd $DIR/../webapp >/dev/null 
+cd $DIR/../webapp >/dev/null
 npm install --quiet
 
-# Run `bower list` for previous `bower list` output
-mkdir -p ${DIR}/../../../target
-filename=${DIR}/../../../target/.webapp-build.$(id -un)
-filename_previous=${DIR}/../../../target/.webapp-build.previous.$(id -un)
-
-touch $filename
-mv $filename $filename_previous
-bower list --offline > $filename
-
-if diff $filename $filename_previous >/dev/null ; then
-  grunt $1
-else
-  grunt deps $1
-fi
+grunt $1
 
 cd - >/dev/null
 

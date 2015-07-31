8200840db22d0a20e1bac507dce6490006fe667c
Aaron McCurry <amccurry@gmail.com>
2015-07-31 14:04:20 -0400

    Updating the csd property generator to have better names (they no longer all start with 'blur') and the installer will no longer be presented with every property during install.

--- blur-util/src/main/java/org/apache/blur/doc/JsonPropertyFormatter.java
+++ blur-util/src/main/java/org/apache/blur/doc/JsonPropertyFormatter.java
@@ -17,27 +17,57 @@
 package org.apache.blur.doc;
 
 import org.apache.blur.doc.BlurPropertyParser.BlurProp;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.common.base.Splitter;
 
 public class JsonPropertyFormatter {
+
+  private static final String BLUR = "blur";
+  private static final String DEFAULT = "default";
+  private static final String TYPE = "type";
+  private static final String CONFIGURABLE_IN_WIZARD = "configurableInWizard";
+  private static final String REQUIRED = "required";
+  private static final String CONFIG_NAME = "configName";
+  private static final String DESCRIPTION = "description";
+  private static final String LABEL = "label";
+  private static final String NAME = "name";
 
   public String separator() {
     return ",";
   }
 
   public String format(BlurProp prop) {
-    return "    {\n" +
-        "      \"name\":\"" + prop.getName().replace(".", "_") + "\",\n" +
-        "      \"label\":\"" + prop.getName().replace(".", " ") + "\",\n" +
-        "      \"description\": \"" + escape(prop.getDescription()) + "\",\n" +
-        "      \"configName\":\"" + prop.getName() + "\",\n" +
-        "      \"required\":\"" + prop.isRequired() + "\",\n" +
-        "      \"type\":\"" + prop.getType() + "\",\n" +
-        "      \"default\":\"" + prop.getDefaultVal() + "\",\n" +
-        "      \"configurableInWizard\":true\n" +
-        "    }\n";
+    JSONObject jsonObject = new JSONObject();
+    try {
+      jsonObject.put(NAME, prop.getName().replace(".", "_"));
+      jsonObject.put(LABEL, pretty(prop.getName()));
+      jsonObject.put(DESCRIPTION, prop.getDescription());
+      jsonObject.put(CONFIG_NAME, prop.getName());
+      jsonObject.put(REQUIRED, prop.isRequired());
+      jsonObject.put(CONFIGURABLE_IN_WIZARD, prop.isRequired());
+      jsonObject.put(TYPE, prop.getType());
+      jsonObject.put(DEFAULT, prop.getDefaultVal());
+      return jsonObject.toString(1);
+    } catch (JSONException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  private String escape(String value) {
-    return value.replace("\"", "'");
+  private String pretty(String s) {
+    Splitter splitter = Splitter.on('.');
+    StringBuilder builder = new StringBuilder();
+    for (String split : splitter.split(s)) {
+      if (builder.length() == 0 && split.equals(BLUR)) {
+        // skip
+      } else {
+        if (builder.length() != 0) {
+          builder.append(' ');
+        }
+        builder.append(split.substring(0, 1).toUpperCase()).append(split.substring(1));
+      }
+    }
+    return builder.toString();
   }
 }
